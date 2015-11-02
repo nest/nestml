b63094e106357e0d72d7fd52b5ada44c681821b8
DimitriPlotnikov <plotnikov@se-rwth.de>
2015-11-02 10:44:21 +0100

    API changes in MontiCore

--- src/main/java/org/nest/symboltable/symbols/NESTMLNeuronSymbolKind.java
+++ src/main/java/org/nest/symboltable/symbols/NESTMLNeuronSymbolKind.java
@@ -14,7 +14,7 @@ import de.monticore.symboltable.SymbolKind;
  * @version $$Revision$$, $$Date$$
  * @since 0.0.1
  */
-public class NESTMLNeuronSymbolKind extends SymbolKind {
+public class NESTMLNeuronSymbolKind implements SymbolKind {
 
   protected NESTMLNeuronSymbolKind() {
   }
