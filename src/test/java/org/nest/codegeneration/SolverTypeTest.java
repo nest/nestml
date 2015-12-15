44f2fe38919991803a961eb7e5fb8ae3e241cf8e
DimitriPlotnikov <plotnikov@se-rwth.de>
2015-12-15 11:19:31 +0100

    Adds new file the for the enum

---
+++ src/test/java/org/nest/codegeneration/SolverTypeTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/
+ */
+package org.nest.codegeneration;
+
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Generates entire NEST implementation for several NESTML models.
+ *
+ * @author plotnikov
+ */
+public class SolverTypeTest {
+  private final static String SOLVER_TYPE_FILE = "src/test/resources/codegeneration/sympy/solverType.property";
+
+  @Test
+  public void testLoadTypeFromFile() {
+    final SolverType result = SolverType.fromFile(Paths.get(SOLVER_TYPE_FILE));
+    assertEquals(SolverType.NUMERIC, result);
+  }
+
+}
