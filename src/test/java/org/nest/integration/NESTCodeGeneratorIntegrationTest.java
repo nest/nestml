4b71907582ba92a7cf9cdfb9e948accbf0004382
DimitriPlotnikov <d.plotnkov@fz-jeulich.de>
2016-04-27 13:20:35 +0200

    Puts imperative models without DSL into own package.

--- src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
+++ src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
@@ -32,12 +32,13 @@ public class NESTCodeGeneratorIntegrationTest extends GenerationBasedTest {
   );
 
   private final List<String> imperativeModels = Lists.newArrayList(
-      "src/test/resources/codegeneration/iaf_tum_2000_imerative.nestml",
-      "src/test/resources/codegeneration/iaf_psc_alpha_imperative.nestml",
-      //"src/test/resources/codegeneration/iaf_psc_exp_multisynapse.nestml",
-      "src/test/resources/codegeneration/mat2_psc_exp.nestml",
-      "src/test/resources/codegeneration/izhikevich.nestml"//,
-      //"src/test/resources/codegeneration/iaf_psc_alpha_multisynapse.nestml"
+      "src/test/resources/codegeneration/imperative/iaf_tum_2000_imerative.nestml",
+      "src/test/resources/codegeneration/imperative/iaf_psc_alpha_multisynapse_imperative.nestml",
+      "src/test/resources/codegeneration/imperative/iaf_psc_delta_imperative.nestml",
+      "src/test/resources/codegeneration/imperative/iaf_psc_exp_imperative.nestml",
+      "src/test/resources/codegeneration/imperative/iaf_psc_exp_multisynapse_imperative.nestml",
+      "src/test/resources/codegeneration/imperative/iaf_tum_2000_imerative.nestml",
+      "src/test/resources/codegeneration/imperative/mat2_psc_exp_imperative.nestml"
   );
 
   private final List<String> nestmlCondModels = Lists.newArrayList(
