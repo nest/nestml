1a22a70255230a349f5b2404458bbc5a6ef95b1b
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2016-06-23 17:59:28 +0200

    - Hormonize neuron models

--- src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
+++ src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
@@ -159,7 +159,7 @@ public class NESTCodeGeneratorIntegrationTest extends GenerationBasedTest {
   @Ignore("Don't run this tests on github")
   @Test
   public void testManually() {
-    final List<String> modelName = Lists.newArrayList("src/test/resources/codegeneration/iaf_tum_2000.nestml");
+    final List<String> modelName = Lists.newArrayList("src/test/resources/codegeneration/iaf_cond_exp_implicit.nestml");
     modelName.forEach(this::checkCocos);
     modelName.forEach(this::invokeCodeGenerator);
     final List<ASTNESTMLCompilationUnit> roots = modelName.stream()
