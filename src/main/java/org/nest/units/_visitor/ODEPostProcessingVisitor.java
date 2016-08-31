554e43d63fe0ffc62cb218bfb14ce88a167d93c9
Philip Traeder <philip.traeder@rwth-aachen.de>
2016-08-31 08:22:26 +0200

    Visitor on ODEDeclarations that calculates the implicit types of variables involved and updates the symboltable.
    Signed-off-by: Philip Traeder <philip.traeder@rwth-aachen.de>

---
+++ src/main/java/org/nest/units/_visitor/ODEPostProcessingVisitor.java
@@ -0,0 +1,85 @@
+package org.nest.units._visitor;
+
+import static de.se_rwth.commons.logging.Log.warn;
+import static org.nest.utils.ASTUtils.convertToSimpleName;
+
+import java.util.Iterator;
+
+import org.nest.nestml._visitor.NESTMLVisitor;
+import org.nest.ode._ast.ASTEquation;
+import org.nest.ode._ast.ASTShape;
+import org.nest.symboltable.predefined.PredefinedTypes;
+import org.nest.symboltable.symbols.TypeSymbol;
+import org.nest.symboltable.symbols.VariableSymbol;
+import org.nest.units.unitrepresentation.UnitRepresentation;
+
+/**
+ * Visitor to ODE Shape and Equation nodes. Calculates implicit type and updates Symbol table.
+ * To be called as soon as symbol table is created.
+ *
+ * @author ptraeder
+ */
+public class ODEPostProcessingVisitor implements NESTMLVisitor {
+
+  public void visit(ASTShape astShape) {
+    if(astShape.getRhs().computeType().get().isError()){
+      warn("Error in Expression type calculation: "+astShape.getRhs().getType().get().getError());
+      return;
+    }
+    final TypeSymbol type = astShape.getRhs().getType().get().getValue();
+
+    if(astShape.getSymbol().isPresent()){
+      final VariableSymbol var = (VariableSymbol) astShape.getSymbol().get();
+      var.setType(type);
+    }
+  }
+
+
+  public void visit(ASTEquation astEquation) {
+    if(astEquation.getRhs().computeType().get().isError()){
+      warn("Error in Expression type calculation: "+astEquation.getRhs().getType().get().getError());
+      return;
+    }
+
+    //TODO: Test if implicit types of expression and derivation from original type match?
+
+    //Calculate type implicit from expression:
+    UnitRepresentation derivativeUnit = new UnitRepresentation(0,0,0,0,0,0,0,0);
+    TypeSymbol typeFromExpression = astEquation.getRhs().getType().get().getValue();
+    if(typeFromExpression.getType() == TypeSymbol.Type.UNIT){
+      derivativeUnit = new UnitRepresentation(typeFromExpression.getName());
+    }
+    derivativeUnit = derivativeUnit.deriveT(astEquation.getLhs().getDifferentialOrder().size());
+    typeFromExpression = PredefinedTypes.getType(derivativeUnit.serialize());
+
+    if(astEquation.getSymbol().isPresent()){
+    final VariableSymbol var = (VariableSymbol) astEquation.getSymbol().get();
+      var.setType(typeFromExpression);
+    }
+  }
+
+  @Override
+  public void traverse(org.nest.ode._ast.ASTOdeDeclaration node) {
+    //TODO: Find a sensible hierarchy for shapes,equations and aliases.
+    {
+      Iterator<org.nest.ode._ast.ASTShape> iter_shapes = node.getShapes().iterator();
+      while (iter_shapes.hasNext()) {
+        iter_shapes.next().accept(getRealThis());
+      }
+    }
+
+    {
+      Iterator<org.nest.ode._ast.ASTEquation> iter_equations = node.getEquations().iterator();
+      while (iter_equations.hasNext()) {
+        iter_equations.next().accept(getRealThis());
+      }
+    }
+
+    {
+      Iterator<org.nest.ode._ast.ASTODEAlias> iter_oDEAliass = node.getODEAliass().iterator();
+      while (iter_oDEAliass.hasNext()) {
+        iter_oDEAliass.next().accept(getRealThis());
+      }
+    }
+  }
+}
