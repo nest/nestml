ceab108517bb16817105ab872f3b07c13b2bc264
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2016-09-13 14:53:12 +0200

    Further cleanup

--- src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
+++ src/test/java/org/nest/integration/NESTCodeGeneratorIntegrationTest.java
@@ -35,8 +35,6 @@ public class NESTCodeGeneratorIntegrationTest extends GenerationBasedTest {
   );
 
   private final List<String> imperativeModels = Lists.newArrayList(
-      "src/test/resources/codegeneration/imperative/iaf_psc_alpha_multisynapse_imperative.nestml",
-      "src/test/resources/codegeneration/imperative/iaf_psc_delta_imperative.nestml",
       "src/test/resources/codegeneration/imperative/iaf_psc_exp_imperative.nestml",
       "src/test/resources/codegeneration/imperative/iaf_psc_exp_multisynapse_imperative.nestml",
       "src/test/resources/codegeneration/imperative/mat2_psc_exp_imperative.nestml"
