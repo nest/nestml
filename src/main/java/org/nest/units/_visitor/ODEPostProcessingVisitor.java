bc403b34497ec10d32f174130a1979a32a967a69
Philip Traeder <philip.traeder@rwth-aachen.de>
2016-10-30 18:36:05 +0100

    Make the OdeVisitor actually do something
    Signed-off-by: Philip Traeder <philip.traeder@rwth-aachen.de>

--- src/main/java/org/nest/units/_visitor/ODEPostProcessingVisitor.java
+++ src/main/java/org/nest/units/_visitor/ODEPostProcessingVisitor.java
@@ -36,12 +36,8 @@ public class ODEPostProcessingVisitor implements NESTMLVisitor {
 
       return;
     }
-    final TypeSymbol type = astShape.getRhs().getType().get().getValue();
+    //TODO: find out what needs to be done here
 
-    if(astShape.getSymbol().isPresent()){
-      final VariableSymbol var = (VariableSymbol) astShape.getSymbol().get();
-      var.setType(type);
-    }
   }
 
 
@@ -56,37 +52,40 @@ public class ODEPostProcessingVisitor implements NESTMLVisitor {
       return;
     }
 
-    //Get Type of original Variable
+    //Resolve LHS Variable
     String varName = astEquation.getLhs().getSimpleName();
     Scope enclosingScope = astEquation.getEnclosingScope().get();
     Optional<VariableSymbol> varSymbol = NESTMLSymbols.resolve(varName,enclosingScope);
 
-    TypeSymbol originalVarType;
-    if(varSymbol.isPresent()){
-       originalVarType = varSymbol.get().getType();
-    }else{
+    TypeSymbol varType;
+    if(!varSymbol.isPresent()){
       warn(ERROR_CODE +" Error while resolving the variable to be derived in ODE: " + varName);
       return;
     }
+    //Derive varType
+    varType = varSymbol.get().getType();
 
+    if(varType.getType() != TypeSymbol.Type.UNIT){
+      warn(ERROR_CODE+ "Type of LHS Variable in ODE is primitive at: "+astEquation.get_SourcePositionStart()+". Skipping.");
+      return;
+    }
 
-    //Calculate LHS type implicit from expression:
-    UnitRepresentation derivativeUnit = new UnitRepresentation(0,0,0,0,0,0,0,0);
+    UnitRepresentation varUnit = new UnitRepresentation(varType.getName());
+    UnitRepresentation derivedVarUnit = varUnit.deriveT(astEquation.getLhs().getDifferentialOrder().size());
+
+    //get type of RHS expression
     TypeSymbol typeFromExpression = astEquation.getRhs().getType().get().getValue();
 
-    if(typeFromExpression.getType() == TypeSymbol.Type.UNIT){
-      derivativeUnit = new UnitRepresentation(typeFromExpression.getName());
-    }
-    derivativeUnit = derivativeUnit.deriveT(astEquation.getLhs().getDifferentialOrder().size());
-    typeFromExpression = PredefinedTypes.getType(derivativeUnit.serialize());
-
-    if(astEquation.getSymbol().isPresent()){
-      final VariableSymbol var = (VariableSymbol) astEquation.getSymbol().get();
-      var.setType(typeFromExpression);
-    }
-    else{
-      warn(ERROR_CODE+ ": Symboltable seems to be missing when running ODEPostProcessingVisitor " + astEquation.get_SourcePositionStart());
+    if(typeFromExpression.getType() != TypeSymbol.Type.UNIT) {
+      warn(ERROR_CODE+ "Type of ODE is primitive at: "+astEquation.get_SourcePositionStart());
       return;
+    }
+    UnitRepresentation unitFromExpression = new UnitRepresentation(typeFromExpression.getName());
+    //do the actual test:
+    if(!unitFromExpression.equals(derivedVarUnit)){
+      warn(ERROR_CODE+ "Type of (derived) variable "+ astEquation.getLhs().toString() + " is: "+ derivedVarUnit.prettyPrint()+
+          ". This doesnt match Type of RHS expression: "+unitFromExpression.prettyPrint()+
+          " at: " +astEquation.get_SourcePositionStart() );
     }
   }
 
