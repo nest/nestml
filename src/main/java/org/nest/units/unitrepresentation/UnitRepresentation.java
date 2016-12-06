fa6e43baef2581913ae66706b79cd59349f3bbdf
Philip Traeder <philip.traeder@rwth-aachen.de>
2016-12-06 10:21:12 +0100

    add space
    Signed-off-by: Philip Traeder <philip.traeder@rwth-aachen.de>

--- src/main/java/org/nest/units/unitrepresentation/UnitRepresentation.java
+++ src/main/java/org/nest/units/unitrepresentation/UnitRepresentation.java
@@ -136,7 +136,7 @@ public class UnitRepresentation implements Comparable<UnitRepresentation>{
 
     //dump magnitude back into the results
     if(abs(this.magnitude)>factors.size()*24){
-      warn(ERROR_CODE+ "Cannot express magnitude "+magnitude+" with only " +(factors.size())+ "factors. (Absolute value of) cumulative magnitude must be <=24.");
+      warn(ERROR_CODE+ "Cannot express magnitude "+magnitude+" with only " +(factors.size())+ " factors. (Absolute value of) cumulative magnitude must be <=24.");
       return("unprintable");
     }
     dumpMagnitude(factors,this.magnitude);
