b64c984360664bc563f00d54e9490edfb67427db
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2017-01-10 22:16:52 +0100

    Add generated code

---
+++ showcase/build/sympyChecker.py
@@ -0,0 +1,5 @@
+import sympy
+from distutils.version import LooseVersion, StrictVersion
+
+output = open('sympyVersion.tmp', 'w')
+output.write(str(LooseVersion(sympy.__version__) > LooseVersion("1.0.1")))
