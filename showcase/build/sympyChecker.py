2408d3255b39115e26da271e55995dcf0436a1f8
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2017-01-11 11:49:40 +0100

    Use different model for the multireceptor usecase

--- showcase/build/sympyChecker.py
+++
@@ -1,5 +0,0 @@
-import sympy
-from distutils.version import LooseVersion, StrictVersion
-
-output = open('sympyVersion.tmp', 'w')
-output.write(str(LooseVersion(sympy.__version__) > LooseVersion("1.0.1")))
