55df51f02767b22d112268c1b23183f1c91506a0
KPerun <konstantin.perun@rwth-aachen.de>
2017-07-30 19:01:16 +0200

    Script for generation of parser/lexer added Expression grammar further written

---
+++ build/src/main/grammars/org/PyNESTMLLexer.py
@@ -0,0 +1,170 @@
+# Generated from src/main/grammars/org/PyNESTML.g4 by ANTLR 4.5.1
+from antlr4 import *
+from io import StringIO
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\2$")
+        buf.write("\u00da\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+        buf.write("\t\36\4\37\t\37\4 \t \4!\t!\4\"\t\"\4#\t#\3\2\3\2\3\3")
+        buf.write("\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3")
+        buf.write("\n\3\n\3\13\3\13\3\13\3\f\3\f\3\f\3\f\3\r\3\r\3\r\3\r")
+        buf.write("\3\16\3\16\3\17\3\17\3\17\3\20\3\20\3\20\3\21\3\21\3\21")
+        buf.write("\3\22\3\22\3\22\3\23\3\23\3\23\3\24\3\24\3\25\3\25\3\25")
+        buf.write("\3\25\3\26\3\26\3\26\3\26\3\27\3\27\3\27\3\30\3\30\3\30")
+        buf.write("\3\31\3\31\3\32\3\32\3\33\3\33\3\34\3\34\3\34\3\34\3\35")
+        buf.write("\3\35\3\35\5\35\u0093\n\35\3\36\3\36\3\36\3\36\3\37\7")
+        buf.write("\37\u009a\n\37\f\37\16\37\u009d\13\37\3\37\7\37\u00a0")
+        buf.write("\n\37\f\37\16\37\u00a3\13\37\3\37\7\37\u00a6\n\37\f\37")
+        buf.write("\16\37\u00a9\13\37\3\37\6\37\u00ac\n\37\r\37\16\37\u00ad")
+        buf.write("\3 \3 \3 \3 \3 \3 \3 \3 \5 \u00b8\n \3!\3!\3!\3!\3!\3")
+        buf.write("!\3!\3!\3!\3!\5!\u00c4\n!\3\"\6\"\u00c7\n\"\r\"\16\"\u00c8")
+        buf.write("\3\"\3\"\6\"\u00cd\n\"\r\"\16\"\u00ce\5\"\u00d1\n\"\3")
+        buf.write("#\3#\3#\3#\3#\3#\5#\u00d9\n#\2\2$\3\3\5\4\7\5\t\6\13\7")
+        buf.write("\r\b\17\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37\21")
+        buf.write("!\22#\23%\24\'\25)\26+\27-\30/\31\61\32\63\33\65\34\67")
+        buf.write("\359\36;\37= ?!A\"C#E$\3\2\6\4\2\f\f\17\17\4\2\13\13\"")
+        buf.write("\"\4\2C\\c|\3\2\62;\u00e4\2\3\3\2\2\2\2\5\3\2\2\2\2\7")
+        buf.write("\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2")
+        buf.write("\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2")
+        buf.write("\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2")
+        buf.write("\2!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2")
+        buf.write("\2\2+\3\2\2\2\2-\3\2\2\2\2/\3\2\2\2\2\61\3\2\2\2\2\63")
+        buf.write("\3\2\2\2\2\65\3\2\2\2\2\67\3\2\2\2\29\3\2\2\2\2;\3\2\2")
+        buf.write("\2\2=\3\2\2\2\2?\3\2\2\2\2A\3\2\2\2\2C\3\2\2\2\2E\3\2")
+        buf.write("\2\2\3G\3\2\2\2\5I\3\2\2\2\7K\3\2\2\2\tM\3\2\2\2\13O\3")
+        buf.write("\2\2\2\rQ\3\2\2\2\17S\3\2\2\2\21U\3\2\2\2\23W\3\2\2\2")
+        buf.write("\25Y\3\2\2\2\27\\\3\2\2\2\31`\3\2\2\2\33d\3\2\2\2\35f")
+        buf.write("\3\2\2\2\37i\3\2\2\2!l\3\2\2\2#o\3\2\2\2%r\3\2\2\2\'u")
+        buf.write("\3\2\2\2)w\3\2\2\2+{\3\2\2\2-\177\3\2\2\2/\u0082\3\2\2")
+        buf.write("\2\61\u0085\3\2\2\2\63\u0087\3\2\2\2\65\u0089\3\2\2\2")
+        buf.write("\67\u008b\3\2\2\29\u0092\3\2\2\2;\u0094\3\2\2\2=\u009b")
+        buf.write("\3\2\2\2?\u00b7\3\2\2\2A\u00c3\3\2\2\2C\u00c6\3\2\2\2")
+        buf.write("E\u00d8\3\2\2\2GH\7)\2\2H\4\3\2\2\2IJ\7*\2\2J\6\3\2\2")
+        buf.write("\2KL\7+\2\2L\b\3\2\2\2MN\7-\2\2N\n\3\2\2\2OP\7\u0080\2")
+        buf.write("\2P\f\3\2\2\2QR\7,\2\2R\16\3\2\2\2ST\7\61\2\2T\20\3\2")
+        buf.write("\2\2UV\7\'\2\2V\22\3\2\2\2WX\7/\2\2X\24\3\2\2\2YZ\7,\2")
+        buf.write("\2Z[\7,\2\2[\26\3\2\2\2\\]\7p\2\2]^\7q\2\2^_\7v\2\2_\30")
+        buf.write("\3\2\2\2`a\7P\2\2ab\7Q\2\2bc\7V\2\2c\32\3\2\2\2de\7>\2")
+        buf.write("\2e\34\3\2\2\2fg\7>\2\2gh\7?\2\2h\36\3\2\2\2ij\7?\2\2")
+        buf.write("jk\7?\2\2k \3\2\2\2lm\7#\2\2mn\7?\2\2n\"\3\2\2\2op\7>")
+        buf.write("\2\2pq\7@\2\2q$\3\2\2\2rs\7@\2\2st\7?\2\2t&\3\2\2\2uv")
+        buf.write("\7@\2\2v(\3\2\2\2wx\7c\2\2xy\7p\2\2yz\7f\2\2z*\3\2\2\2")
+        buf.write("{|\7C\2\2|}\7P\2\2}~\7F\2\2~,\3\2\2\2\177\u0080\7q\2\2")
+        buf.write("\u0080\u0081\7t\2\2\u0081.\3\2\2\2\u0082\u0083\7Q\2\2")
+        buf.write("\u0083\u0084\7T\2\2\u0084\60\3\2\2\2\u0085\u0086\7A\2")
+        buf.write("\2\u0086\62\3\2\2\2\u0087\u0088\7.\2\2\u0088\64\3\2\2")
+        buf.write("\2\u0089\u008a\7<\2\2\u008a\66\3\2\2\2\u008b\u008c\7g")
+        buf.write("\2\2\u008c\u008d\7p\2\2\u008d\u008e\7f\2\2\u008e8\3\2")
+        buf.write("\2\2\u008f\u0090\7\17\2\2\u0090\u0093\7\f\2\2\u0091\u0093")
+        buf.write("\t\2\2\2\u0092\u008f\3\2\2\2\u0092\u0091\3\2\2\2\u0093")
+        buf.write(":\3\2\2\2\u0094\u0095\t\3\2\2\u0095\u0096\3\2\2\2\u0096")
+        buf.write("\u0097\b\36\2\2\u0097<\3\2\2\2\u0098\u009a\7a\2\2\u0099")
+        buf.write("\u0098\3\2\2\2\u009a\u009d\3\2\2\2\u009b\u0099\3\2\2\2")
+        buf.write("\u009b\u009c\3\2\2\2\u009c\u00a1\3\2\2\2\u009d\u009b\3")
+        buf.write("\2\2\2\u009e\u00a0\t\4\2\2\u009f\u009e\3\2\2\2\u00a0\u00a3")
+        buf.write("\3\2\2\2\u00a1\u009f\3\2\2\2\u00a1\u00a2\3\2\2\2\u00a2")
+        buf.write("\u00a7\3\2\2\2\u00a3\u00a1\3\2\2\2\u00a4\u00a6\7a\2\2")
+        buf.write("\u00a5\u00a4\3\2\2\2\u00a6\u00a9\3\2\2\2\u00a7\u00a5\3")
+        buf.write("\2\2\2\u00a7\u00a8\3\2\2\2\u00a8\u00ab\3\2\2\2\u00a9\u00a7")
+        buf.write("\3\2\2\2\u00aa\u00ac\t\4\2\2\u00ab\u00aa\3\2\2\2\u00ac")
+        buf.write("\u00ad\3\2\2\2\u00ad\u00ab\3\2\2\2\u00ad\u00ae\3\2\2\2")
+        buf.write("\u00ae>\3\2\2\2\u00af\u00b0\7v\2\2\u00b0\u00b1\7t\2\2")
+        buf.write("\u00b1\u00b2\7w\2\2\u00b2\u00b8\7g\2\2\u00b3\u00b4\7V")
+        buf.write("\2\2\u00b4\u00b5\7t\2\2\u00b5\u00b6\7w\2\2\u00b6\u00b8")
+        buf.write("\7g\2\2\u00b7\u00af\3\2\2\2\u00b7\u00b3\3\2\2\2\u00b8")
+        buf.write("@\3\2\2\2\u00b9\u00ba\7h\2\2\u00ba\u00bb\7c\2\2\u00bb")
+        buf.write("\u00bc\7n\2\2\u00bc\u00bd\7u\2\2\u00bd\u00c4\7g\2\2\u00be")
+        buf.write("\u00bf\7H\2\2\u00bf\u00c0\7c\2\2\u00c0\u00c1\7n\2\2\u00c1")
+        buf.write("\u00c2\7u\2\2\u00c2\u00c4\7g\2\2\u00c3\u00b9\3\2\2\2\u00c3")
+        buf.write("\u00be\3\2\2\2\u00c4B\3\2\2\2\u00c5\u00c7\t\5\2\2\u00c6")
+        buf.write("\u00c5\3\2\2\2\u00c7\u00c8\3\2\2\2\u00c8\u00c6\3\2\2\2")
+        buf.write("\u00c8\u00c9\3\2\2\2\u00c9\u00d0\3\2\2\2\u00ca\u00cc\7")
+        buf.write("\60\2\2\u00cb\u00cd\t\5\2\2\u00cc\u00cb\3\2\2\2\u00cd")
+        buf.write("\u00ce\3\2\2\2\u00ce\u00cc\3\2\2\2\u00ce\u00cf\3\2\2\2")
+        buf.write("\u00cf\u00d1\3\2\2\2\u00d0\u00ca\3\2\2\2\u00d0\u00d1\3")
+        buf.write("\2\2\2\u00d1D\3\2\2\2\u00d2\u00d3\7k\2\2\u00d3\u00d4\7")
+        buf.write("p\2\2\u00d4\u00d9\7h\2\2\u00d5\u00d6\7K\2\2\u00d6\u00d7")
+        buf.write("\7p\2\2\u00d7\u00d9\7h\2\2\u00d8\u00d2\3\2\2\2\u00d8\u00d5")
+        buf.write("\3\2\2\2\u00d9F\3\2\2\2\16\2\u0092\u009b\u00a1\u00a7\u00ad")
+        buf.write("\u00b7\u00c3\u00c8\u00ce\u00d0\u00d8\3\2\3\2")
+        return buf.getvalue()
+
+
+class PyNESTMLLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    T__8 = 9
+    T__9 = 10
+    T__10 = 11
+    T__11 = 12
+    T__12 = 13
+    T__13 = 14
+    T__14 = 15
+    T__15 = 16
+    T__16 = 17
+    T__17 = 18
+    T__18 = 19
+    T__19 = 20
+    T__20 = 21
+    T__21 = 22
+    T__22 = 23
+    T__23 = 24
+    T__24 = 25
+    BLOCK_START = 26
+    BLOCK_END = 27
+    NEWLINE = 28
+    WS = 29
+    STRING_LITERAL = 30
+    BOOLEAN_TRUE_LITERAL = 31
+    BOOLEAN_FALSE_LITERAL = 32
+    NUMERIC_LITERAL = 33
+    INF_LITERAL = 34
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'''", "'('", "')'", "'+'", "'~'", "'*'", "'/'", "'%'", "'-'", 
+            "'**'", "'not'", "'NOT'", "'<'", "'<='", "'=='", "'!='", "'<>'", 
+            "'>='", "'>'", "'and'", "'AND'", "'or'", "'OR'", "'?'", "','", 
+            "':'", "'end'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "BLOCK_START", "BLOCK_END", "NEWLINE", "WS", "STRING_LITERAL", 
+            "BOOLEAN_TRUE_LITERAL", "BOOLEAN_FALSE_LITERAL", "NUMERIC_LITERAL", 
+            "INF_LITERAL" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "T__8", "T__9", "T__10", "T__11", "T__12", "T__13", 
+                  "T__14", "T__15", "T__16", "T__17", "T__18", "T__19", 
+                  "T__20", "T__21", "T__22", "T__23", "T__24", "BLOCK_START", 
+                  "BLOCK_END", "NEWLINE", "WS", "STRING_LITERAL", "BOOLEAN_TRUE_LITERAL", 
+                  "BOOLEAN_FALSE_LITERAL", "NUMERIC_LITERAL", "INF_LITERAL" ]
+
+    grammarFileName = "PyNESTML.g4"
+
+    def __init__(self, input=None):
+        super().__init__(input)
+        self.checkVersion("4.5.1")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
--- src/main/grammars/org/PyNESTML.g4
+++ src/main/grammars/org/PyNESTML.g4
@@ -6,44 +6,50 @@ grammar PyNESTML;
 
 /*************************************************************************
 ************************TOKENS DEFINTION**********************************/
-
 /**
 * The block start and finish tokens are used to indicate units of declaration.
+* Example:
+*   state:
+*    ...
+*   end
 */
 BLOCK_START : ':' ;
 
 BLOCK_END : 'end' ;
 
-NEWLINE = ('\r' '\n' | '\r' | '\n' );
+NEWLINE : ('\r' '\n' | '\r' | '\n' );
 
-WS = (' ' | '\t')->channel(HIDDEN);
+WS : (' ' | '\t')->channel(HIDDEN);
 
 /**
 * In order to reduce problems during parsing we use only a limited alphabet and the underscore.
-* TODO: here we should no allow literals consisting only of _ .
+* We do not allow strings which consist only of the underscore, since it could lead to undesired naming of variables.
+* Example:
+*  (1) __D_g_in
 */
-STRING_LITERAL : [a-zA-Z\\_];
+STRING_LITERAL : ('_')* [a-zA-Z]* ('_')* [a-zA-Z]+;
 
 /**
 * Boolean values, i.e., true and false, should be handled as tokens in order to enable handling of lower
-* and upper case definitions.
+* and upper case definitions. Here, we allow both concepts, the python like syntax starting with upper case and
+* the concept as currently used in NESTML with the lower case.
 */
-BOOLEAN_TRUE_LITERAL : 'true' | 'TRUE' ;
-BOOLEAN_FALSE_LITERAL : 'false' | 'FALSE' ;
-
+BOOLEAN_TRUE_LITERAL : 'true' | 'True' ;
+BOOLEAN_FALSE_LITERAL : 'false' | 'False' ;
 
 /**
 * Numeric literals. We allow integers as well as floats.
 * Example:
-*   1, 3.14, ....
+*  (1) 1
+*  (2) 3.14
 */
 
-NUMERIC_LITERAL : [0-9]+(\.[0-9]+)?;
+NUMERIC_LITERAL : [0-9]+ ('.' [0-9]+)?;
 
-/*
+/**
 * The infinity element is represented by the keyword inf. We allow lower or upper case keywords.
 */
-INF_LITERAL : ("inf"|"INF");
+INF_LITERAL : ('inf'|'Inf');
 
 
 /*************************************************************************
@@ -53,7 +59,7 @@ INF_LITERAL : ("inf"|"INF");
 * Example:
 *   g_in''
 */
-Variable : var=STRING_LITERAL (order='\'')*;
+variable : var=STRING_LITERAL (order='\'')*;
 
 /**
 * An expression can be (1)simple, i.e. a numeric or alphabetical value with a signum, a (2)compound expression, i.e., consisting of two components
@@ -63,61 +69,66 @@ Variable : var=STRING_LITERAL (order='\'')*;
 * (2) V_m - 65mV, exp(10mV + V_m)
 * (3) (2+2)**2
 */
-Expression : SimpleExpression | CompoundExpression | leftBracket='(' Expression leftBracket='(';
+expression : simpleExpression | compoundExpression | leftBracket='(' expression rightBracket=')';
 
 /**
 * Simple expression are those composed of a signum and a numeric value or reference. Moreover, it is allowed to used boolean literals here.
-* TODO: What about the unary tilde?
-*
+* Example:
+* (1) -10mV
+* TODO: we need to support units.
 */
-SimpleExpression : (unaryPlus='+' | unaryMinus='+')? (var=Variable | num=NUMERIC_LITERAL | inf=INF_LITERAL ) | BOOLEAN_TRUE_LITERAL | BOOLEAN_FALSE_LITERAL;
+simpleExpression : (unaryPlus='+' | unaryMinus='+' | unaryTilde='~')? (var=variable | num=NUMERIC_LITERAL | inf=INF_LITERAL ) | BOOLEAN_TRUE_LITERAL | BOOLEAN_FALSE_LITERAL;
 
 /**
 * A compound expression can be either:
-* - a power expression, e.g., 2**2.
-* - a point expression (times,div,modulo), e.g., 2*2.
-* - a TODOExpression (plus,minus), e.g., 2-2
+* (1) a power expression, e.g., 2**2.
+* (2) an arithmetic expression, e.g., 2+2, 2*2
+* (3) a logical expression, e.g., x == y
+* (4) a function call, e.g., myfunc(10ms)
 */
-CompoundExpression : PowerExpression | PointExpression | TODOExpression | LogicalExpression | FunctionCall;
+compoundExpression : arithmeticExpression | logicalExpression | functionCall;
 
-/**
-* A power expression consisting of a base and the corresponding exponent.
-*/
-
-PowerExpression : <assorc=right> base=Expression "**" exponent=Expression;
 
 /**
 * An arithmetic expression is a combination of a two expression by an arithmetic operator.
 * Example:
-*   10mV + V_m*2
+*  (1) 10mV + V_m*2
+*  (2) 10mV**2
 */
-ArithmeticExpression : lhs=Expression (timesOp='*'|divOp='/' | modOp='%' | plusOp='+' | minusOp='-') rhs=Expression;
+arithmeticExpression : lhs=expression (timesOp='*'| divOp='/' | modOp='%' ) rhs=expression
+                     | lhs=expression (plusOp='+' | minusOp='-') rhs=expression
+                     | <assoc=right> base=expression powOp='**' exponent=expression
+                     ;
 
 /**
 * Logic expressions are expressions which consisting of two sub-expression combined with a logic operator or a relation, the negation of a expression or the ternary
 * operator.
 * Examples:
-*   10mV > V_m, not r == 0, V_m > 50mV? V_m = 50mV: V_m = V_m
+*  (1) 10mV > V_m
+*  (2) not r == 0
+*  (3) V_m > 50mV? V_m = 50mV: V_m = V_m
 */
-LogicalExpression : lhs=Expression (lt="<"|leq="<="|eq="=="|neq=("!="|"<>")|geq=">="|gt=">") rhs=Expression
-                  | logNot=("not"|"NOT") expr=Expression
-                  | lhs=Expression logAnd=("and"|"AND") rhs=Expression
-                  | lhs=Expression logOr=("or"|"OR") rhs=Expression
-                  | condition=Expression '?' ifTrue=Expression ':' ifNotTrue=Expression
+logicalExpression : logNot=('not'|'NOT') expr=expression
+                  | lhs=expression (lt='<'|leq='<='|eq='=='|neq=('!='|'<>')|geq='>='|gt='>') rhs=expression
+                  | lhs=expression logAnd=('and'|'AND') rhs=expression
+                  | lhs=expression logOr=('or'|'OR') rhs=expression
+                  | condition=expression '?' ifTrue=expression ':' ifNotTrue=expression
                   ;
 
 /**
 * Function calls represent calls to predefined (mathematical) or simulator specific functions.
 * Example:
-*  exp(10), steps(10ms), myFunc(10mV,1ms)
+* (1) exp(10)
+* (2) steps(10ms)
+* (3) myFunc(10mV,1ms)
 */
-FunctionCall : functionName=STRING_LITERAL '(' (args=Parameter)? ')';
+functionCall : functionName=STRING_LITERAL '(' (args=parameter)? ')';
 
 /**
 * If a function calls contains any parameters, it should consist of at leas one parameter expression, and if subsequent parameters are there, then
 * separated by a comma.
 * Example:
-*   (10mV)
-*   (10mV,2mV)
+* (1) (10mV)
+* (2) (10mV,2mV)
 */
-Parameter: Expression (',' Expression)*;
+parameter: expression (',' expression)*;
