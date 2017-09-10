15f76f4c5ddbaf64a0eb37b80d269a2838f6e130
KPerun <konstantin.perun@rwth-aachen.de>
2017-09-10 17:09:08 +0200

    New test added for testing of special blocks (for/while)

---
+++ pynestml/src/test/python/SpecialBlockParserBuilderTest.py
@@ -0,0 +1,49 @@
+"""
+/*
+ *  SpecialBlockParserBuilderTest.py
+ *
+ *  This file is part of NEST.
+ *
+ *  Copyright (C) 2004 The NEST Initiative
+ *
+ *  NEST is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NEST is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ @author kperun
+"""
+
+import unittest
+from antlr4 import *
+import os
+from pynestml.src.main.python.org.nestml.parser.NESTMLParser import NESTMLParser
+
+
+class SpecialBlockParserBuilderTest(unittest.TestCase):
+    """
+    This text is used to check the parsing of special blocks, i.e. for and while-blocks, is executed as expected
+    and the corresponding AST built correctly.
+    """
+
+    def test(self):
+        return
+        print('Start special block parsing and AST-building test...'),
+        model = NESTMLParser.parseModel(
+            os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'resources')),
+                         'BlockTest.nestml'))
+        print('done')
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
