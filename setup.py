b95372be943ef82e37f273ebc0ab2a69989e57dc
Konstantin <k.perun@fz-juelich.de>
2018-06-28 16:41:52 +0200

    Update setup to include templates

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import sys
 
 setup(
     name='PyNESTML',
-    version='0.1.1',
+    version='0.1.2',
     description='NestML is a domain specific language that supports the specification of neuron models in a'
                 ' precise and concise syntax, based on the syntax of Python. Model equations can either be given'
                 ' as a simple string of mathematical notation or as an algorithm written in the built-in procedural'
@@ -31,6 +31,10 @@ setup(
     license='GNU General Public License v2.0',
     url='https://github.com/nest/nestml/tree/PyNestML',
     packages=find_packages(),
+    package_dir={'codegeneration': 'pynestml/codegeneration'},
+    package_data={'codegeneration': ['resources_NEST/*.jinja2',
+                                     'resources_NEST/directives/*jinja2,'
+                                     'resources_NEST/setup/*.jinja2']},
     install_requires=['numpy >= 1.8.2',
                       'sympy >= 1.1.1',
                       ('antlr4-python2-runtime' if sys.version_info.major == 2 else
