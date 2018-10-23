a97894836d2a9072b404f23a5c2664a780dae417
C.A.P. Linssen <charl@turingbirds.com>
2018-10-23 17:28:32 +0200

    updated the version printing

--- pynestml/frontend/frontend_configuration.py
+++ pynestml/frontend/frontend_configuration.py
@@ -67,13 +67,14 @@ class FrontendConfiguration(object):
         :type args: list(str)
         """
         cls.argument_parser = argparse.ArgumentParser(
-                description='NESTML is a domain specific language that supports the specification of neuron models in a'
-                            ' precise and concise syntax, based on the syntax of Python. Model equations can either be '
-                            ' given as a simple string of mathematical notation or as an algorithm written in the '
-                            'built-in '
-                            ' procedural language. The equations are analyzed by NESTML to compute an exact solution'
-                            ' if possible or use an appropriate numeric solver otherwise.'
-                            ' Version '  + str(pynestml.__version__))
+                description='''NESTML is a domain specific language that supports the specification of neuron
+models in a precise and concise syntax, based on the syntax of Python. Model
+equations can either be given as a simple string of mathematical notation or
+as an algorithm written in the built-in procedural language. The equations are
+analyzed by NESTML to compute an exact solution if possible or use an
+appropriate numeric solver otherwise.
+
+ Version ''' + str(pynestml.__version__), formatter_class=argparse.RawDescriptionHelpFormatter)
 
         cls.argument_parser.add_argument(qualifier_path_arg, type=str, nargs='+',
                                          help=help_path)
