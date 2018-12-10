e05ec8d4fb859eeee093d21c405c30e4a5f9127f
C.A.P. Linssen <charl@turingbirds.com>
2018-12-10 19:00:15 +0100

    updates to the models_coverage_checker: dealing with _neuron suffix

--- extras/models_coverage_checker.py
+++ extras/models_coverage_checker.py
@@ -1,8 +1,21 @@
+import copy
+import numpy as np
 import nest
+
+def strip_suffix(l, suffix):
+	new_l = []
+	for n in l:
+		if n[-len(suffix):] == suffix:
+			n = n[:-len(suffix)]
+		new_l.append(n)
+	return new_l
+
 
 models = nest.Models(mtype="nodes")
 neuron_models = [m for m in models if str(nest.GetDefaults(m, "element_type")) == "neuron"]
+_neuron_models = strip_suffix(neuron_models, "_neuron")
 syn_models = nest.Models(mtype="synapses")
+
 
 print("Before: had " + str(len(neuron_models)) + " neuron models")
 
@@ -17,8 +30,9 @@ print("Before: had " + str(len(neuron_models)) + " neuron models")
 
 from pynestml.frontend.pynestml_frontend import to_nest, install_nest
 
-#to_nest(input_path="/home/archels/julich/nestml-fork/nestml/models/aeif_cond_alpha.nestml", target_path="/tmp/work_pynestml/target")
-to_nest(input_path="/home/archels/julich/nestml-fork/nestml/models/iaf_psc_exp.nestml", target_path="/tmp/nestml-target", dev=True, suffix="_nestml")
+#to_nest(input_path="/home/archels/julich/nestml-fork/nestml/models/aeif_cond_alpha.nestml", target_path="/tmp/nestml-target", suffix="_nestml", logging_level="INFO")
+#to_nest(input_path="/home/archels/julich/nestml-fork/nestml/models/ht_neuron.nestml", target_path="/tmp/nestml-target", suffix="_nestml")
+to_nest(input_path="/home/archels/julich/nestml-fork/nestml/models", target_path="/tmp/nestml-target", suffix="_nestml")
 
 install_nest("/tmp/nestml-target", "/home/archels/julich/nest-simulator-build")
 
@@ -35,6 +49,59 @@ nest.Install("nestmlmodule")
 
 new_models = nest.Models(mtype="nodes")
 new_neuron_models = [m for m in new_models if str(nest.GetDefaults(m, "element_type")) == "neuron"]
+_new_neuron_models = strip_suffix(new_neuron_models, "_nestml")
+_new_neuron_models = strip_suffix(_new_neuron_models, "_neuron")
 new_syn_models = nest.Models(mtype="synapses")
 
 print("After: have " + str(len(new_neuron_models)) + " neuron models")
+
+all_models = np.sort(list(set(_neuron_models) | set(_new_neuron_models)))
+
+fname = "/tmp/models_coverage.html"
+html_file = open(fname, "w")
+html_file.write("""
+<meta charset="UTF-8">
+<html>
+<head>
+<style>
+td { padding: 3px; font-weight: bold }
+.header td { background-color:#ddeeff; padding: 5px !important }
+.check_yes {background-color: #00ff33 }
+.check_no {background-color: #dd3300 }
+tr { margin-bottom: 5px; margin-top: 5px }
+tr:nth-child(even) { background: #eee }
+tr:nth-child(odd) { background: #fff }
+</style>
+</head>
+<body>
+""")
+html_file.write("<table>")
+html_file.write("\t<tr class=\"header\"><td>Model name</td><td>NEST?</td><td>NESTML?</td><td>Behavioural</td></tr>")
+
+for i, n in enumerate(all_models):
+	html_file.write("\t<tr>")
+
+	html_file.write("\t<td>" + n + "</td>")
+
+	if n in neuron_models or n + "_neuron" in neuron_models:
+		html_file.write("\t<td class=\"check_yes\">✓</td>")
+	else:
+		html_file.write("\t<td class=\"check_no\">✗</td>")
+
+	if n + "_nestml" in new_neuron_models or n + "_neuron_nestml" in new_neuron_models:
+		html_file.write("\t<td class=\"check_yes\">✓</td>")
+	else:
+		html_file.write("\t<td class=\"check_no\">✗</td>")
+
+	html_file.write("\t<td>?</td>")
+
+	html_file.write("\t</tr>")
+
+html_file.write("</table>")
+
+html_file.write(str(len(all_models)) + " models total")
+html_file.write("</body></html>")
+
+html_file.close()
+
+print("Wrote " + fname)
