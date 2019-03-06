3dc426a2325564cefcc864b7c07eff9a5cc6098c
C.A.P. Linssen <charl@turingbirds.com>
2019-03-06 13:05:27 +0100

    add test for warning when variable is declared with same name as physical unit

--- tests/unit_system_test.py
+++ tests/unit_system_test.py
@@ -123,6 +123,15 @@ class UnitSystemTest(unittest.TestCase):
         from astropy import units as u
         self.assertTrue(declaration.get_expression().type.unit.unit == u.mV)
 
+    def test_declaration_with_same_variable_name_as_unit(self):
+        model = ModelParser.parse_model(
+            os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), 'resources')),
+                         'DeclarationWithSameVariableNameAsUnit.nestml'))
+        self.assertEqual(len(
+            Logger.get_all_messages_of_level_and_or_neuron(model.get_neuron_list()[0], LoggingLevel.ERROR)), 0)
+        self.assertEqual(len(
+            Logger.get_all_messages_of_level_and_or_neuron(model.get_neuron_list()[0], LoggingLevel.WARNING)), 1)
+
     def test_expression_after_magnitude_conversion_in_standalone_function_call(self):
         model = ModelParser.parse_model(
             os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), 'resources')),
