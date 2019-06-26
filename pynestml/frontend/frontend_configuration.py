6a04e13d7bdfc3bd83e094c5b6171d175e5adff7
C.A.P. Linssen <charl@turingbirds.com>
2019-06-26 15:01:29 +0200

    fix logic for handling input paths and derivation of module name

--- pynestml/frontend/frontend_configuration.py
+++ pynestml/frontend/frontend_configuration.py
@@ -19,6 +19,7 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 import argparse  # used for parsing of input arguments
 import os
+import re
 
 import pynestml
 from pynestml.codegeneration.codegenerator import CodeGenerator
@@ -88,34 +89,42 @@ appropriate numeric solver otherwise.
         cls.argument_parser.add_argument(qualifier_suffix_arg, metavar='SUFFIX', type=str, help=help_suffix, default='')
         cls.argument_parser.add_argument(qualifier_dev_arg, action='store_true', help=help_dev)
         parsed_args = cls.argument_parser.parse_args(args)
-        # get the source path
-        cls.handle_source_path(parsed_args.input_path[0])
 
         # initialize the logger
         if parsed_args.logging_level is not None:
             cls.logging_level = parsed_args.logging_level
             Logger.init_logger(Logger.string_to_level(parsed_args.logging_level))
         else:
-            cls.logging_level = "ERROR"
-            Logger.init_logger(Logger.string_to_level("ERROR"))
-        # now update the target
+            cls.logging_level = 'ERROR'
+            Logger.init_logger(Logger.string_to_level('ERROR'))
+
+        cls.handle_input_path(parsed_args.input_path[0])
         cls.handle_target(parsed_args.target)
-        # now update the target path
         cls.handle_target_path(parsed_args.target_path)
-        # now adjust the name of the module, if it is a single file, then it is called just module
+
+        # parse or compose the module name
         if parsed_args.module_name is not None:
-            assert parsed_args.module_name.endswith('module'), "Module name (\"" + parsed_args.module_name + "\") should end with \"module\""
+            if not parsed_args.module_name.endswith('module'):
+                raise Exception('Invalid module name specified ("' + parsed_args.module_name + '"): the module name should end with the word "module"')
             cls.module_name = parsed_args.module_name
         elif os.path.isfile(parsed_args.input_path[0]):
             cls.module_name = 'nestmlmodule'
+            Logger.log_message(message='No module name specified; the generated module will be named "' + cls.module_name + '"', log_level=LoggingLevel.INFO)
         elif os.path.isdir(parsed_args.input_path[0]):
             cls.module_name = os.path.basename(os.path.normpath(parsed_args.input_path[0]))
+            if not re.match('[a-zA-Z_][a-zA-Z0-9_]*\Z', cls.module_name):
+                raise Exception('No module name specified; tried to use the input directory name ("' + cls.module_name + '"), but it cannot be parsed as a C variable name')
+
+            if not cls.module_name.endswith('module'):
+                cls.module_name += 'module'
+
+            Logger.log_message(message='No module name specified; the generated module will be named "' + cls.module_name + '"', log_level=LoggingLevel.INFO)
         else:
-            cls.module_name = 'nestmlmodule'
+            assert False # input_path should be either a file or a directory; failure should have been caught already by handle_input_path()
+
         cls.store_log = parsed_args.store_log
         cls.suffix = parsed_args.suffix
         cls.is_dev = parsed_args.dev
-        return
 
     @classmethod
     def get_path(cls):
@@ -182,8 +191,8 @@ appropriate numeric solver otherwise.
 
     @classmethod
     def handle_target(cls, target):
-        if target is None or target.upper() == "NONE":
-            target = ""     # make sure `target` is always a string
+        if target is None or target.upper() == 'NONE':
+            target = ''     # make sure `target` is always a string
 
         if target not in CodeGenerator.get_known_targets():
             code, message = Messages.get_unknown_target(target)
@@ -204,30 +213,33 @@ appropriate numeric solver otherwise.
                 cls.target_path = os.path.join(pynestml_dir, path)
         else:
             cls.target_path = os.path.join(os.getcwd(), 'target')
-            Logger.log_message(log_level=LoggingLevel.INFO, message="Target files will be generated in directory \"" + cls.target_path + "\"")
+            Logger.log_message(log_level=LoggingLevel.INFO, message='Target files will be generated in directory \'' + cls.target_path + '\'')
         # check if the target path dir already exists
         if not os.path.isdir(cls.target_path):
             os.makedirs(cls.target_path)
 
     @classmethod
-    def handle_source_path(cls, path):
-        # check if a target has been selected, otherwise set the buildNest as target
-        if path is None:
+    def handle_input_path(cls, path):
+        if path is None or path == '':
             # check if the mandatory path arg has been handed over, just terminate
-            raise InvalidPathException('Invalid source path!')
+            raise InvalidPathException('No input path specified.')
+
+        cls.paths_to_compilation_units = list()
+        if os.path.isabs(path):
+            cls.provided_path = path
         else:
-            cls.paths_to_compilation_units = list()
-            if os.path.isabs(path):
-                cls.provided_path = path
             # a relative path, reconstruct it. get the parent dir where models, pynestml etc. is located
-            else:
-                pynestml_dir = os.getcwd()
-                cls.provided_path = os.path.join(pynestml_dir, path)
-            if os.path.isfile(cls.provided_path):
-                cls.paths_to_compilation_units.append(cls.provided_path)
-            elif os.path.isdir(cls.provided_path):
-                for filename in os.listdir(cls.provided_path):
-                    if filename.endswith(".nestml"):
-                        cls.paths_to_compilation_units.append(os.path.join(cls.provided_path, filename))
-            else:
-                cls.paths_to_compilation_units = cls.provided_path
+            pynestml_dir = os.getcwd()
+            cls.provided_path = os.path.join(pynestml_dir, path)
+
+        if os.path.isfile(cls.provided_path):
+            cls.paths_to_compilation_units.append(cls.provided_path)
+        elif os.path.isdir(cls.provided_path):
+            for filename in os.listdir(cls.provided_path):
+                if filename.endswith('.nestml'):
+                    cls.paths_to_compilation_units.append(os.path.join(cls.provided_path, filename))
+        else:
+            # input_path should be either a file or a directory
+            code, message = Messages.get_input_path_not_found(path=cls.provided_path)
+            Logger.log_message(message=message, log_level=LoggingLevel.ERROR)
+            raise Exception(message)
