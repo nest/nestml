8bd206546f197ca49ce3c7c9afda142ab4166816
C.A.P. Linssen <c.linssen@fz-juelich.de>
2019-08-06 13:42:29 +0200

    enhance NEST integration test by comparing NESTML-generated against existing NEST C++ models behaviourally

--- tests/nest_tests/nest_integration_test.py
+++ tests/nest_tests/nest_integration_test.py
@@ -20,10 +20,12 @@
 
 import nest
 
-TEST_PLOTS = True
-if TEST_PLOTS:
+try:
     import matplotlib
     import matplotlib.pyplot as plt
+    TEST_PLOTS = True
+except:
+    TEST_PLOTS = False
 
 nest.set_verbosity("M_ALL")
 nest.Install("nestmlmodule")
@@ -75,7 +77,7 @@ def test(referenceModel, testant, gsl_error_tol, tolerance=0.000001):
         for _ax in ax:
             _ax.legend(loc='upper right')
             _ax.grid()
-        plt.show()
+        plt.savefig("/tmp/nestml_nest_integration_test_[" + referenceModel + "]_[" + testant + "].png")
 
     for index in range(0, len(Vms1)):
         if abs(Vms1[index] - Vms2[index]) > tolerance:
@@ -88,33 +90,32 @@ def test(referenceModel, testant, gsl_error_tol, tolerance=0.000001):
 if __name__ == "__main__":
     # execute only if run as a script
     models = list()
-    """models.append(("iaf_chxk_2008", "iaf_chxk_2008_implicit", 1.e-3, 0.001))
-    models.append(("iaf_chxk_2008", "iaf_chxk_2008_neuron", 1.e-3, 0.001))
-    models.append(("aeif_cond_alpha", "aeif_cond_alpha_implicit", 1.e-3, 0.001))
-    models.append(("aeif_cond_alpha", "aeif_cond_alpha_neuron", 1.e-3, 0.001))
-    models.append(("aeif_cond_exp", "aeif_cond_exp_implicit", 1.e-3, 0.001))
-    models.append(("aeif_cond_exp", "aeif_cond_exp_neuron", 1.e-3, 0.001))
-    models.append(("hh_cond_exp_traub", "hh_cond_exp_traub_implicit", 1.e-3, 0.001))
-    models.append(("hh_cond_exp_traub", "hh_cond_exp_traub_neuron", 1.e-3, 0.001))
-    models.append(("hh_psc_alpha", "hh_psc_alpha_implicit", 1.e-3, 0.001))
-    models.append(("hh_psc_alpha", "hh_psc_alpha_neuron", 1.e-3, 0.001))"""
+    models.append(("iaf_chxk_2008", "iaf_chxk_2008_implicit_nestml", 1.e-3, 0.001))
+    models.append(("iaf_chxk_2008", "iaf_chxk_2008_nestml", 1.e-3, 0.001))
+    models.append(("aeif_cond_alpha", "aeif_cond_alpha_implicit_nestml", 1.e-3, 0.001))
+    models.append(("aeif_cond_alpha", "aeif_cond_alpha_nestml", 1.e-3, 0.001))
+    models.append(("aeif_cond_exp", "aeif_cond_exp_implicit_nestml", 1.e-3, 0.001))
+    models.append(("aeif_cond_exp", "aeif_cond_exp_nestml", 1.e-3, 0.001))
+    models.append(("hh_cond_exp_traub", "hh_cond_exp_traub_implicit_nestml", 1.e-3, 0.001))
+    models.append(("hh_cond_exp_traub", "hh_cond_exp_traub_nestml", 1.e-3, 0.001))
+    models.append(("hh_psc_alpha", "hh_psc_alpha_implicit_nestml", 1.e-3, 0.001))
+    models.append(("hh_psc_alpha", "hh_psc_alpha_nestml", 1.e-3, 0.001))
     models.append(("iaf_cond_alpha", "iaf_cond_alpha_nestml", 1E-3, 1E-3))
     models.append(("iaf_cond_alpha", "iaf_cond_alpha_implicit_nestml", 1E-3, 1E-3))
-    """models.append(("iaf_cond_beta_neuron", "iaf_cond_beta_neuron", None, 0.001))
-    models.append(("iaf_cond_exp", "iaf_cond_exp_neuron", 1.e-3, 0.001))
-    models.append(("iaf_cond_exp", "iaf_cond_exp_implicit", 1.e-3, 0.001))
-    models.append(("iaf_cond_exp_sfa_rr", "iaf_cond_exp_sfa_rr_neuron", 1.e-3, 0.001))
-    models.append(("iaf_cond_exp_sfa_rr", "iaf_cond_exp_sfa_rr_implicit", 1.e-3, 0.001))
-    models.append(("iaf_neuron", "iaf_neuron_nestml", None, 0.001))
-    models.append(("iaf_psc_alpha", "iaf_psc_alpha_neuron", None, 0.001))
+    """models.append(("iaf_cond_beta_neuron", "iaf_cond_beta_nestml", None, 0.001))
+    models.append(("iaf_cond_exp", "iaf_cond_exp_nestml", 1.e-3, 0.001))
+    models.append(("iaf_cond_exp", "iaf_cond_exp_implicit_nestml", 1.e-3, 0.001))
+    models.append(("iaf_cond_exp_sfa_rr", "iaf_cond_exp_sfa_rr_nestml", 1.e-3, 0.001))
+    models.append(("iaf_cond_exp_sfa_rr", "iaf_cond_exp_sfa_rr_implicit_nestml", 1.e-3, 0.001))
+    models.append(("iaf_psc_alpha", "iaf_psc_alpha_nestml", None, 0.001))
     models.append(("iaf_psc_delta", "iaf_psc_delta", None, 0.001))
-    models.append(("iaf_psc_exp", "iaf_psc_exp_neuron", None, 0.01))
-    models.append(("iaf_tum_2000", "iaf_tum_2000_neuron", None, 0.01))
-    models.append(("izhikevich", "izhikevich_neuron", 1.e-3, 0.5))
-    models.append(("izhikevich_psc_alpha", "izhikevich_psc_alpha_implicit", 1.e-3, 0.1))
-    models.append(("mat2_psc_exp", "mat2_psc_exp_neuron", None, 0.1))
-    models.append(("terub_neuron_gpe", "terub_neuron_gpe_implicit", 1.e-3, 0.001))
-    models.append(("terub_neuron_stn", "terub_neuron_stn_implicit", 1.e-3, 0.001))"""
+    models.append(("iaf_psc_exp", "iaf_psc_exp_nestml", None, 0.01))
+    models.append(("iaf_tum_2000", "iaf_tum_2000_nestml", None, 0.01))
+    models.append(("izhikevich", "izhikevich_nestml", 1.e-3, 0.5))
+    models.append(("izhikevich_psc_alpha", "izhikevich_psc_alpha_implicit_nestml", 1.e-3, 0.1))
+    models.append(("mat2_psc_exp", "mat2_psc_exp_nestml", None, 0.1))
+    models.append(("terub_neuron_gpe", "terub_neuron_gpe_implicit_nestml", 1.e-3, 0.001))
+    models.append(("terub_neuron_stn", "terub_neuron_stn_implicit_nestml", 1.e-3, 0.001))"""
 
     for reference, testant, gsl_error_tol, tollerance in models:
         test(reference, testant, gsl_error_tol, tollerance)
