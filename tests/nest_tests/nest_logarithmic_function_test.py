38bfe8f364b66be14200eed846a39c093e88dbf9
C. Linssen <c.linssen@fz-juelich.de>
2019-09-03 16:56:40 +0200

    add natural logarithm ln() to predefined functions and rename base-10 log function to log10()

---
+++ tests/nest_tests/nest_logarithmic_function_test.py
@@ -0,0 +1,98 @@
+#
+# nest_logarithmic_function_test.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Sanity test for the predefined logarithmic functions ln() and log10()"""
+
+import os
+import nest
+import numpy as np
+from pynestml.frontend.pynestml_frontend import to_nest, install_nest
+
+try:
+    import matplotlib
+    import matplotlib.pyplot as plt
+    TEST_PLOTS = True
+except:
+    TEST_PLOTS = False
+
+
+
+if __name__ == "__main__":
+
+    input_path = os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), "resources")))
+    nest_path = "/home/travis/nest_install"
+    target_path = 'target'
+    logging_level = 'INFO'
+    module_name = 'nestmlmodule'
+    store_log = False
+    suffix = '_nestml'
+    dev = True
+    to_nest(input_path, target_path, logging_level, module_name, store_log, suffix, dev)
+    install_nest(target_path, nest_path)
+    nest.set_verbosity("M_ALL")
+
+    nest.ResetKernel()
+    nest.Install("nestmlmodule")
+
+    nrn = nest.Create("logarithm_function_test_nestml")
+    mm = nest.Create('multimeter')
+
+    ln_state_specifier = 'ln_state'
+    log10_state_specifier = 'log10_state'
+    nest.SetStatus(mm, {"withtime": True, "record_from": [ln_state_specifier, log10_state_specifier, "x"]})
+
+    nest.Connect(mm, nrn)
+
+    nest.Simulate(100.0)
+
+    dmm = nest.GetStatus(mm)[0]
+    timevec = dmm["events"]["x"]
+    ln_state_ts = dmm["events"][ln_state_specifier]
+    log10_state_ts = dmm["events"][log10_state_specifier]
+    ref_ln_state_ts = np.log(timevec - 1)
+    ref_log10_state_ts = np.log10(timevec - 1)
+
+    assert np.all(ln_state_ts == ref_ln_state_ts)
+    assert np.all(log10_state_ts == ref_log10_state_ts)
+
+    # test that expected failure occurs
+
+    nest.ResetKernel()
+    nrn = nest.Create("logarithm_function_test_invalid_nestml")
+
+    mm = nest.Create('multimeter')
+
+    ln_state_specifier = 'ln_state'
+    log10_state_specifier = 'log10_state'
+    nest.SetStatus(mm, {"withtime": True, "record_from": [ln_state_specifier, log10_state_specifier, "x"]})
+
+    nest.Connect(mm, nrn)
+
+    nest.Simulate(100.0)
+
+    dmm = nest.GetStatus(mm)[0]
+    timevec = dmm["events"]["x"]
+    ln_state_ts = dmm["events"][ln_state_specifier]
+    log10_state_ts = dmm["events"][log10_state_specifier]
+    ref_ln_state_ts = np.log(timevec - 1)
+    ref_log10_state_ts = np.log10(timevec - 1)
+
+    assert not np.all(ln_state_ts == ref_ln_state_ts)
+    assert not np.all(log10_state_ts == ref_log10_state_ts)
