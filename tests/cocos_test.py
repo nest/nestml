deb4c109f226252ed7c9943013af53823204ffe4
C.A.P. Linssen <charl@turingbirds.com>
2019-12-04 11:26:33 +0100

    update sum function mention in comments and model names to convolve function

--- tests/cocos_test.py
+++ tests/cocos_test.py
@@ -422,7 +422,7 @@ class CoCosTest(unittest.TestCase):
         Logger.set_logging_level(LoggingLevel.INFO)
         model = ModelParser.parse_model(
             os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), 'invalid')),
-                         'CoCoSumNotCorrectlyParametrized.nestml'))
+                         'CoCoConvolveNotCorrectlyParametrized.nestml'))
         self.assertEqual(len(
             Logger.get_all_messages_of_level_and_or_neuron(model.get_neuron_list()[0], LoggingLevel.ERROR)), 2)
 
@@ -430,7 +430,7 @@ class CoCosTest(unittest.TestCase):
         Logger.set_logging_level(LoggingLevel.INFO)
         model = ModelParser.parse_model(
             os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), 'valid')),
-                         'CoCoSumNotCorrectlyParametrized.nestml'))
+                         'CoCoConvolveNotCorrectlyParametrized.nestml'))
         self.assertEqual(len(Logger.get_all_messages_of_level_and_or_neuron(model.get_neuron_list()[0],
                                                                             LoggingLevel.ERROR)), 0)
 
