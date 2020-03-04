b35bcd05b710966d3b7a695b8285064f82d96f2b
C.A.P. Linssen <charl@turingbirds.com>
2020-03-04 12:59:48 +0100

    fix logo in top left corner

--- doc/sphinx-apidoc/conf.py
+++ doc/sphinx-apidoc/conf.py
@@ -197,7 +197,7 @@ html_logo = "_images/nestml-logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_images']
+html_static_path = ['_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
