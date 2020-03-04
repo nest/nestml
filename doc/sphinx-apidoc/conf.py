f9672754819c6e595df82a80727894eeed2fbbe3
C.A.P. Linssen <charl@turingbirds.com>
2020-03-04 12:10:31 +0100

    fix logo in top left corner

--- doc/sphinx-apidoc/conf.py
+++ doc/sphinx-apidoc/conf.py
@@ -191,7 +191,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo_only': True}
+html_logo = "https://raw.githubusercontent.com/nest/nestml/master/doc/nestml-logo/nestml-logo.png"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
