8ca48e5de8075eb733c8f4ccc69e82408f90dc1d
C. Linssen <c.linssen@fz-juelich.de>
2020-05-27 16:52:07 +0200

    remove unrelated fixes from PR

--- pynestml/visitors/comment_collector_visitor.py
+++ pynestml/visitors/comment_collector_visitor.py
@@ -17,7 +17,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
-
 from pynestml.generated.PyNestMLParserVisitor import PyNestMLParserVisitor
 
 
