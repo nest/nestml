a1848b026e20688416bd79397fc074963273951f
clinssen <c.linssen@fz-juelich.de>
2020-08-14 23:52:33 +0200

    Update pynestml/cocos/co_co_convolve_cond_correctly_built.py
    Co-authored-by: Tanguy Fardet <Silmathoron@users.noreply.github.com>

--- pynestml/cocos/co_co_convolve_cond_correctly_built.py
+++ pynestml/cocos/co_co_convolve_cond_correctly_built.py
@@ -26,7 +26,7 @@ from pynestml.visitors.ast_visitor import ASTVisitor
 
 class CoCoConvolveCondCorrectlyBuilt(CoCo):
     """
-    This coco ensures that convolve function call correctly built, i.e., that the first argument is the variable from the initial block and the second argument an input buffer.
+    This coco ensures that ``convolve`` is correctly called, i.e. that the first argument is the variable from the initial block and the second argument is an input buffer.
 
     Allowed:
         inline I_syn_exc pA = convolve(g_ex, spikesExc) * ( V_m - E_ex )
