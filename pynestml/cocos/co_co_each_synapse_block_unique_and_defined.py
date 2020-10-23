99eca25c9a332754944e19343372501a8dd646f3
C. Linssen <c.linssen@fz-juelich.de>
2020-10-23 11:22:25 +0200

    make all blocks optional in synapse models

--- pynestml/cocos/co_co_each_synapse_block_unique_and_defined.py
+++ pynestml/cocos/co_co_each_synapse_block_unique_and_defined.py
@@ -51,8 +51,8 @@ class CoCoEachSynapseBlockUniqueAndDefined(CoCo):
             code, message = Messages.get_block_not_defined_correctly('State', False)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
                                log_level=LoggingLevel.ERROR)
-        # check that no update block is defined
-        if isinstance(node.get_update_blocks(), list) and len(node.get_update_blocks()) > 0:
+        # check that update block is defined at most once
+        if isinstance(node.get_update_blocks(), list) and len(node.get_update_blocks()) > 1:
             code, message = Messages.get_block_not_defined_correctly('Update', False)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
                                log_level=LoggingLevel.ERROR)
@@ -71,7 +71,7 @@ class CoCoEachSynapseBlockUniqueAndDefined(CoCo):
             code, message = Messages.get_block_not_defined_correctly('Equations', False)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
                                log_level=LoggingLevel.ERROR)
-        # check that input block is defined exactly once
+        # check that input block is defined at most once
         if isinstance(node.get_input_blocks(), list) and len(node.get_input_blocks()) > 1:
             code, message = Messages.get_block_not_defined_correctly('Input', False)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
@@ -79,12 +79,12 @@ class CoCoEachSynapseBlockUniqueAndDefined(CoCo):
         elif isinstance(node.get_input_blocks(), list) and len(node.get_input_blocks()) == 0:
             code, message = Messages.get_block_not_defined_correctly('Input', True)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
-                               log_level=LoggingLevel.ERROR)
+                               log_level=LoggingLevel.WARNING)
         elif node.get_input_blocks() is None:
             code, message = Messages.get_block_not_defined_correctly('Input', True)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
-                               log_level=LoggingLevel.ERROR)
-        # check that output block is defined exactly once
+                               log_level=LoggingLevel.WARNING)
+        # check that output block is defined at most once
         if isinstance(node.get_output_blocks(), list) and len(node.get_output_blocks()) > 1:
             code, message = Messages.get_block_not_defined_correctly('Output', False)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
@@ -92,9 +92,8 @@ class CoCoEachSynapseBlockUniqueAndDefined(CoCo):
         elif isinstance(node.get_output_blocks(), list) and len(node.get_output_blocks()) == 0:
             code, message = Messages.get_block_not_defined_correctly('Output', True)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
-                               log_level=LoggingLevel.ERROR)
+                               log_level=LoggingLevel.WARNING)
         elif node.get_output_blocks() is None:
             code, message = Messages.get_block_not_defined_correctly('Output', True)
             Logger.log_message(code=code, message=message, astnode=node, error_position=node.get_source_position(),
-                               log_level=LoggingLevel.ERROR)
-        return
+                               log_level=LoggingLevel.WARNING)
