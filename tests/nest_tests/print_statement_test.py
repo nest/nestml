50610b3f9251ce0e894c511408f797e52812fab8
Pooja Babu <p.babu@fz.juelich.de>
2021-01-25 13:36:04 +0100

    Fix copyright header

--- tests/nest_tests/print_statement_test.py
+++ tests/nest_tests/print_statement_test.py
@@ -6,7 +6,7 @@
 #
 # Copyright (C) 2004 The NEST Initiative
 #
-# NEST is free software: you can,  redistribute it and/or modify
+# NEST is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
