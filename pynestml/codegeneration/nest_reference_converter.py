7662d4185df63b7691f90d216f0a84f3cb04b7aa
Pooja Babu <p.babu@fz.juelich.de>
2021-02-04 14:26:54 +0100

    Fix docstrings

--- pynestml/codegeneration/nest_reference_converter.py
+++ pynestml/codegeneration/nest_reference_converter.py
@@ -269,12 +269,19 @@ class NESTReferenceConverter(IReferenceConverter):
 
         .. code-block:: nestml
 
-        print("Hello World")
-        Converted NEST: std::cout << "Hello World";
+            print("Hello World")
 
         .. code-block:: C++
-        print("Membrane potential = {V_m}")
-        Converted NEST: std::cout << "Membrane potential = " << V_m << " mV";
+
+            std::cout << "Hello World";
+
+        .. code-block:: nestml
+
+            print("Membrane potential = {V_m}")
+
+        .. code-block:: C++
+
+            std::cout << "Membrane potential = " << V_m << " mV";
 
         :param stmt: argument to the print or println function
         :type stmt: str
