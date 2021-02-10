3ccd6f87afd3543e0d89e94c7f1f35498d59f228
C.A.P. Linssen <charl@turingbirds.com>
2021-02-10 14:52:58 +0100

    fix missing import

--- pynestml/frontend/pynestml_frontend.py
+++ pynestml/frontend/pynestml_frontend.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any, Mapping, Optional, Union
+from typing import Any, Mapping, Optional, Sequence, Union
 
 import os
 import sys
