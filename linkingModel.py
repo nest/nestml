4dd9efefaf050e9fe0f544677ff32b63de0449b1
name <name@localhostroot.com>
2021-02-25 17:01:36 +0100

    creating iaf_test.nestml for experimental purposes linkingModel.py made little more practical remove generated files from being tracked

--- linkingModel.py
+++ linkingModel.py
@@ -1,10 +1,47 @@
+import os
+
 from pynestml.frontend.pynestml_frontend import to_nest, install_nest
 
-to_nest(input_path="/home/name/thesis/eclipse/workspace/nestml/models/iaf_psc_exp.nestml", target_path="/home/name/eclipse-thesis/workspace/nestml/generated")
+# string = "python setup.py install --user"
+# os.system(string)
 
-install_nest("/home/name/thesis/eclipse/workspace/nestml/generated", "/home/name/thesis/nest-simulator/build_master_nompi/install")
+# /home/name/thesis/eclipse/workspace/nestml
+# sandbox.run_setup('setup.py', ['install', '--user'])
+
+
+THESIS_HOME = "/home/name/thesis"
+
+NESTML_HOME_SUFFIX = "/eclipse/workspace/nestml"
+NESTSIM_INSTALL_SUFFIX = "/nest-simulator/build_master_nompi/install"
+
+NESTML_HOME = THESIS_HOME + NESTML_HOME_SUFFIX
+NESTSIM_HOME = THESIS_HOME + NESTSIM_INSTALL_SUFFIX
+
+
+# to_nest(input_path=os.path.join(NESTML_HOME, "models/iaf_test.nestml"), 
+        # target_path=os.path.join(NESTML_HOME, "generated"))
+
+to_nest(input_path=os.path.join(NESTML_HOME, "models/iaf_psc_exp.nestml"), 
+        target_path=os.path.join(NESTML_HOME, "generated"))
+
+install_nest(os.path.join(NESTML_HOME, "generated"), NESTSIM_HOME)
 
 nest.Install("nestmlmodule")
 nest.Create("cm_main")
 # ...
 #nest.Simulate(400.)
+
+#rmtree(os.path.join(NESTML_HOME, "generated"))
+
+
+
+
+
+
+
+
+
+
+
+
+
