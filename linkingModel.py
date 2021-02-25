a6f63fa89dd0683a903e2a0e16d1ba98a294888b
name <name@localhostroot.com>
2021-02-25 18:05:06 +0100

    linking script improved for more convenient debugging and experimenting

--- linkingModel.py
+++ linkingModel.py
@@ -1,12 +1,7 @@
 import os
+from shutil import rmtree 
 
 from pynestml.frontend.pynestml_frontend import to_nest, install_nest
-
-# string = "python setup.py install --user"
-# os.system(string)
-
-# /home/name/thesis/eclipse/workspace/nestml
-# sandbox.run_setup('setup.py', ['install', '--user'])
 
 
 THESIS_HOME = "/home/name/thesis"
@@ -17,21 +12,37 @@ NESTSIM_INSTALL_SUFFIX = "/nest-simulator/build_master_nompi/install"
 NESTML_HOME = THESIS_HOME + NESTML_HOME_SUFFIX
 NESTSIM_HOME = THESIS_HOME + NESTSIM_INSTALL_SUFFIX
 
+GEN_DIR = os.path.join(NESTML_HOME, "generated")
+MODEL_FILE = os.path.join(NESTML_HOME, "models/iaf_psc_exp.nestml")
 
-# to_nest(input_path=os.path.join(NESTML_HOME, "models/iaf_test.nestml"), 
-        # target_path=os.path.join(NESTML_HOME, "generated"))
+#cleanup
+try:
+    rmtree(GEN_DIR)
+except OSError:
+    print ("Cleaning up %s failed" % GEN_DIR)
+else:
+    print ("Successfully deleted the %s and its contents" % GEN_DIR)
 
-to_nest(input_path=os.path.join(NESTML_HOME, "models/iaf_psc_exp.nestml"), 
-        target_path=os.path.join(NESTML_HOME, "generated"))
+#fresh dir
+try:
+    os.mkdir(GEN_DIR)
+except OSError:
+    print ("Creation of directory %s failed" % GEN_DIR)
+else:
+    print ("Successfully created directory %s " % GEN_DIR)
+    
+to_nest(input_path=MODEL_FILE, target_path=GEN_DIR)
+install_nest(GEN_DIR, NESTSIM_HOME)
 
-install_nest(os.path.join(NESTML_HOME, "generated"), NESTSIM_HOME)
 
-nest.Install("nestmlmodule")
-nest.Create("cm_main")
-# ...
-#nest.Simulate(400.)
 
-#rmtree(os.path.join(NESTML_HOME, "generated"))
+
+# inside nest it would be
+# nest.Install("nestmlmodule")
+# nest.Create("cm_main")
+# nest.Simulate(400.)
+#...
+
 
 
 
