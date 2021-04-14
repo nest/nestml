7ec0f1b8401531b253af96ce3794658da57c8798
Pooja Babu <p.babu@fz.juelich.de>
2021-04-14 13:57:37 +0200

    Fix adding recordables to DynamicRecordablesMap
    - Change the recordableMap variable from static to class member
    - Add recordableMap.create() to the copy conctructor in case of
    DynamicRecordablesMap
    - Update unit test

--- tests/nest_tests/nest_vectors_test.py
+++ tests/nest_tests/nest_vectors_test.py
@@ -20,6 +20,7 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import unittest
+import numpy as np
 
 import nest
 
@@ -40,7 +41,7 @@ class NestVectorsIntegrationTest(unittest.TestCase):
         store_log = False
         suffix = '_nestml'
         dev = True
-        # to_nest(input_path, target_path, logging_level, module_name, store_log, suffix, dev)
+        to_nest(input_path, target_path, logging_level, module_name, store_log, suffix, dev)
         install_nest(target_path, nest_path)
         nest.set_verbosity("M_ALL")
 
@@ -53,7 +54,6 @@ class NestVectorsIntegrationTest(unittest.TestCase):
         recordables.extend(["G_IN_" + str(i + 1) for i in range(0, 20)])
         recordables.extend(["G_EX_" + str(i + 1) for i in range(0, 10)])
         recordables.append("V_m")
-        print(recordables)
         multimeter.set({"record_from": recordables})
         nest.Connect(multimeter, neuron)
 
@@ -63,6 +63,9 @@ class NestVectorsIntegrationTest(unittest.TestCase):
         g_in = events["G_IN_1"]
         g_ex = events["G_EX_2"]
         print("g_in: {}, g_ex: {}".format(g_in, g_ex))
+        np.testing.assert_almost_equal(g_in[-1], 11.)
+        np.testing.assert_almost_equal(g_ex[-1], -2.)
 
         v_m = multimeter.get("events")["V_m"]
         print("V_m: {}".format(v_m))
+        np.testing.assert_almost_equal(v_m[-1], -0.3)
