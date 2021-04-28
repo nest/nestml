d9a5655d9d5fc74393ad3d28fb1bb2b4a94551e3
C.A.P. Linssen <charl@turingbirds.com>
2021-04-28 19:41:28 +0200

    remove unrelated files

--- runtests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#LD_LIBRARY_PATH=/home/archels/nestml-fork-jit/nestml:/home/archels/nest-simulator-build/lib/nest:/home/archels/nestml-fork-jit/nestml/target PYTHONPATH=$PYTHONPATH:/home/archels/ode-toolbox:/home/archels/nest-simulator-build/lib/python3.5/site-packages ipython3 -m pytest -- -s --pdb tests/nest_tests/stdp_synapse_test.py
-LD_LIBRARY_PATH=/home/archels/nestml-fork-jit/nestml:/home/archels/nest-simulator-build/lib/nest:/tmp/nestml-stdp PYTHONPATH=$PYTHONPATH:/home/archels/ode-toolbox:/home/archels/nest-simulator-build/lib/python3.5/site-packages ipython3 -m pytest -- -s --pdb tests/nest_tests/stdp_synapse_test.py
-
