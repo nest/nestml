378959d6db7f9368396262a4bf5f40b184891bbf
C.A.P. Linssen <charl@turingbirds.com>
2021-05-31 21:07:46 +0200

    fix type checking of compound assignments

--- pynestml/cocos/co_co_all_variables_defined.py
+++ pynestml/cocos/co_co_all_variables_defined.py
@@ -71,18 +71,18 @@ class CoCoAllVariablesDefined(CoCo):
                     # in this case its ok if it is recursive or defined later on
                     continue
 
-                # now check if it has been defined before usage, except for predefined symbols, buffers and variables added by the AST transformation functions
+                # now check if it has been defined before usage, except for predefined symbols, input ports and variables added by the AST transformation functions
                 elif (not symbol.is_predefined) \
                         and symbol.block_type != BlockType.INPUT_BUFFER_CURRENT \
                         and symbol.block_type != BlockType.INPUT_BUFFER_SPIKE \
                         and not symbol.get_referenced_object().get_source_position().is_added_source_position():
                     # except for parameters, those can be defined after
                     if ((not symbol.get_referenced_object().get_source_position().before(var.get_source_position()))
-                            and (not symbol.block_type in [BlockType.PARAMETERS, BlockType.INTERNALS])):
+                            and (not symbol.block_type in [BlockType.PARAMETERS, BlockType.INTERNALS, BlockType.STATE])):
                         code, message = Messages.get_variable_used_before_declaration(var.get_name())
                         Logger.log_message(neuron=node, message=message, error_position=var.get_source_position(),
                                            code=code, log_level=LoggingLevel.ERROR)
-                        # now check that they are now defined recursively, e.g. V_m mV = V_m + 1
+                    # now check that they are not defined recursively, e.g. V_m mV = V_m + 1
                     # todo: we should not check this for invariants
                     if (symbol.get_referenced_object().get_source_position().encloses(var.get_source_position())
                             and not symbol.get_referenced_object().get_source_position().is_added_source_position()):
