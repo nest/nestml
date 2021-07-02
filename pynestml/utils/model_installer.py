be397dd385952c31c2b95279b726e1a62e98ffb4
C.A.P. Linssen <charl@turingbirds.com>
2021-07-02 17:37:50 +0200

    implement GeneratedCodeBuildException for exceptions during generated code build stage

--- pynestml/utils/model_installer.py
+++ pynestml/utils/model_installer.py
@@ -18,27 +18,40 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import subprocess
 import sys
 
+from pynestml.exceptions.generated_code_build_exception import GeneratedCodeBuildException
 
-def install_nest(models_path, nest_path):
+
+def install_nest(target_path: str, nest_path: str) -> None:
     """
-    This method can be used to install all models located in the ${models} dir into NEST. For the simulator,
-    the path to the installation has to be provided (a.k.a. the -Dwith-nest argument of the make command).
-    Caution: The nest_path should only point to the install dir, the suffix  /bin/nest-config is automatically attached.
+    This method can be used to build the generated code and install the resulting extension module into NEST.
+
+    Parameters
+    ----------
+    target_path : str
+        Path to the target directory, which should contain the generated code artifacts (target platform code and CMake configuration file).
+    nest_path : str
+        Path to the NEST installation, which should point to the main directory where NEST is installed. This folder contains the ``bin``, ``lib(64)``, ``include``, and ``share`` folders of the NEST install. The ``bin`` folder should contain the ``nest-config`` script, which is accessed by NESTML to perform the installation. This path is the same as that passed through the ``-Dwith-nest`` argument of the CMake command before building the generated NEST module. The suffix ``bin/nest-config`` will be automatically appended to ``nest_path``.
+
+    Raises
+    ------
+    GeneratedCodeBuildException
+        If any kind of failure occurs during cmake configuration, build, or install.
     """
-    if not os.path.isdir(models_path):
-        print('PyNestML: Models path not a directory (%s)! abort installation...' % models_path)
-        return
+    if not os.path.isdir(target_path):
+        raise GeneratedCodeBuildException('Models path (' + target_path + ') is not a directory!')
+
     if not os.path.isdir(nest_path):
-        print('PyNestML: NEST path not a directory (%s)! abort installation...' % nest_path)
-        return
+        raise GeneratedCodeBuildException('NEST path (' + nest_path + ') is not a directory!')
 
-    cmake_cmd = ['cmake', '-Dwith-nest=' + str(nest_path) + '/bin/nest-config', '.']
+    cmake_cmd = ['cmake', '-Dwith-nest=' + os.path.join(nest_path, 'bin', 'nest-config'), '.']
     make_all_cmd = ['make', 'all']
     make_install_cmd = ['make', 'install']
+
     # check if we run on win
     if sys.platform.startswith('win'):
         shell = True
@@ -48,26 +61,20 @@ def install_nest(models_path, nest_path):
     # first call cmake with all the arguments
     try:
         result = subprocess.check_call(cmake_cmd, stderr=subprocess.STDOUT, shell=shell,
-                                       cwd=str(os.path.join(models_path)))
+                                       cwd=str(os.path.join(target_path)))
     except subprocess.CalledProcessError as e:
-        print('PyNestML: Something went wrong in \'cmake\', see error above!')
-        print('abort installation...')
-        return
+        raise GeneratedCodeBuildException('Error occurred during \'cmake\'! More detailed error messages can be found in stdout.')
 
     # now execute make all
     try:
         subprocess.check_call(make_all_cmd, stderr=subprocess.STDOUT, shell=shell,
-                              cwd=str(os.path.join(models_path)))
+                              cwd=str(os.path.join(target_path)))
     except subprocess.CalledProcessError as e:
-        print('PyNestML: Something went wrong in \'make all\', see error above!')
-        print('abort installation...')
-        return
+        raise GeneratedCodeBuildException('Error occurred during \'make all\'! More detailed error messages can be found in stdout.')
 
     # finally execute make install
     try:
         subprocess.check_call(make_install_cmd, stderr=subprocess.STDOUT, shell=shell,
-                              cwd=str(os.path.join(models_path)))
+                              cwd=str(os.path.join(target_path)))
     except subprocess.CalledProcessError as e:
-        print('PyNestML: Something went wrong in \'make install\', see error above!')
-        print('abort installation...')
-        return
+        raise GeneratedCodeBuildException('Error occurred during \'make install\'! More detailed error messages can be found in stdout.')
