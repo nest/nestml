da3885dc3d500c5d1dce6bbd355e9e5472aa862c
ayssar:ubuntu_pc <med.ayssar@gmail.com>
2022-01-13 13:02:07 +0100

    Missing copyrigt header

--- pynestml/codegeneration/nest_cpp_printer.py
+++ pynestml/codegeneration/nest_cpp_printer.py
@@ -1,5 +1,26 @@
-from json import load
-from posix import environ
+# -*- coding: utf-8 -*-
+#
+# nest_cpp_printer.py
+#
+# This file is part of NEST.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from typing import Any, List, Mapping, Optional, Sequence
 import pynestml
 from pynestml.meta_model.ast_function import ASTFunction
