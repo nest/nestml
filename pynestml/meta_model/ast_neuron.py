f9f3d19eb739aa89ed9627d16f6ca41e1225cce1
ayssar:ubuntu_pc <med.ayssar@gmail.com>
2022-02-10 17:10:43 +0100

    Use SymbolTableVisitor in the ASTNeuron.clone

--- pynestml/meta_model/ast_neuron.py
+++ pynestml/meta_model/ast_neuron.py
@@ -76,13 +76,17 @@ class ASTNeuron(ASTNeuronOrSynapse):
                         artifact_name=self.artifact_name,
                         # ASTNode common attributes:
                         source_position=self.source_position,
-                        scope=self.scope.clone(),
+                        scope=self.scope,
                         comment=self.comment,
                         pre_comments=[s for s in self.pre_comments],
                         in_comment=self.in_comment,
                         post_comments=[s for s in self.post_comments],
                         implicit_conversion_factor=self.implicit_conversion_factor)
 
+        from pynestml.visitors.ast_symbol_table_visitor import ASTSymbolTableVisitor
+        symtable_vistor = ASTSymbolTableVisitor()
+        symtable_vistor = ASTSymbolTableVisitor()
+        dup.accept(symtable_vistor)
         return dup
 
     def get_name(self):
