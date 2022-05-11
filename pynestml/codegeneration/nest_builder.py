3bd26e9ea1f19f74ef0932e6fec110ffa2cf7e36
C.A.P. Linssen <charl@turingbirds.com>
2022-05-11 09:59:58 +0200

    use Mac & Windows-compatible way to get number of CPU cores available

--- pynestml/codegeneration/nest_builder.py
+++ pynestml/codegeneration/nest_builder.py
@@ -123,7 +123,11 @@ class NESTBuilder(Builder):
                 install_path = os.path.abspath(install_path)
             install_prefix = f"-DCMAKE_INSTALL_PREFIX={install_path}"
 
-        n_cpu = len(os.sched_getaffinity(0))  # compile multithreaded
+        # compile multithreaded -- how many threads?
+        try:
+            n_cpu = len(os.sched_getaffinity(0))  # only available on Linux
+        except AttributeError:
+            n_cpu = os.cpu_count()
 
         nest_config_path = f"-Dwith-nest={os.path.join(nest_path, 'bin', 'nest-config')}"
         cmake_cmd = ['cmake', nest_config_path, install_prefix, '.']
