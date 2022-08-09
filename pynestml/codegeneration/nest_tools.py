24c0d78c5d731caf436e09f7022435604e7270ed
C.A.P. Linssen <charl@turingbirds.com>
2022-08-08 22:02:21 -0700

    improve NEST version detection and compatibility

--- pynestml/codegeneration/nest_tools.py
+++ pynestml/codegeneration/nest_tools.py
@@ -38,12 +38,27 @@ class NESTTools:
         """
         try:
             import nest
+
+            vt = nest.Create("volume_transmitter")
+
+            try:
+                neuron = nest.Create('hh_psc_alpha_clopath')
+            except:
+                pass
+
             if "DataConnect" in dir(nest):
                 nest_version = "v2.20.2"
-            elif "kernel_status" not in dir(nest):
+            elif "kernel_status" not in dir(nest):  # added in v3.1
                 nest_version = "v3.0"
-            else:
+            elif "prepared" in nest.GetKernelStatus().keys():  # "prepared" key was added after v3.3 release
                 nest_version = "master"
+            elif "tau_u_bar_minus" in neuron.get().keys():   # added in v3.3
+                nest_version = "v3.3"
+            elif "tau_Ca" in vt.get().keys():   # removed in v3.2
+                nest_version = "v3.1"
+            else:
+                nest_version = "v3.2"
+
         except ModuleNotFoundError:
             Logger.log_message(None, -1, "An error occurred while importing the `nest` module in Python. Please check your NEST installation-related environment variables and paths, or specify ``nest_version`` manually in the code generator options.", None, LoggingLevel.ERROR)
             sys.exit(1)
