5f54d1777ace1600351606c4e7cb14c7714e09dd
C.A.P. Linssen <charl@turingbirds.com>
2022-08-09 23:22:44 -0700

    run NEST version detection in a separate process

--- pynestml/codegeneration/nest_tools.py
+++ pynestml/codegeneration/nest_tools.py
@@ -19,10 +19,41 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
+import multiprocessing as mp
 import sys
 
 from pynestml.utils.logger import Logger
 from pynestml.utils.logger import LoggingLevel
+
+
+def _detect_nest_version(user_args):
+    try:
+        import nest
+
+        vt = nest.Create("volume_transmitter")
+
+        try:
+            neuron = nest.Create('hh_psc_alpha_clopath')
+        except Exception:
+            pass
+
+        if "DataConnect" in dir(nest):
+            nest_version = "v2.20.2"
+        elif "kernel_status" not in dir(nest):  # added in v3.1
+            nest_version = "v3.0"
+        elif "prepared" in nest.GetKernelStatus().keys():  # "prepared" key was added after v3.3 release
+            nest_version = "master"
+        elif "tau_u_bar_minus" in neuron.get().keys():   # added in v3.3
+            nest_version = "v3.3"
+        elif "tau_Ca" in vt.get().keys():   # removed in v3.2
+            nest_version = "v3.1"
+        else:
+            nest_version = "v3.2"
+
+    except ModuleNotFoundError:
+        nest_version = ""
+
+    return nest_version
 
 
 class NESTTools:
@@ -32,34 +63,18 @@ class NESTTools:
     def detect_nest_version(cls) -> str:
         r"""Auto-detect NEST Simulator installed version. The returned string corresponds to a git tag or git branch name.
 
+        Do this in a separate process to avoid potential side-effects of import the ``nest`` Python module.
+
         .. admonition::
 
            NEST version detection needs improvement. See https://github.com/nest/nest-simulator/issues/2116
         """
-        try:
-            import nest
 
-            vt = nest.Create("volume_transmitter")
+        p = mp.Pool(processes=1)
+        nest_version = p.map(_detect_nest_version, [None])[0]
+        p.close()
 
-            try:
-                neuron = nest.Create('hh_psc_alpha_clopath')
-            except Exception:
-                pass
-
-            if "DataConnect" in dir(nest):
-                nest_version = "v2.20.2"
-            elif "kernel_status" not in dir(nest):  # added in v3.1
-                nest_version = "v3.0"
-            elif "prepared" in nest.GetKernelStatus().keys():  # "prepared" key was added after v3.3 release
-                nest_version = "master"
-            elif "tau_u_bar_minus" in neuron.get().keys():   # added in v3.3
-                nest_version = "v3.3"
-            elif "tau_Ca" in vt.get().keys():   # removed in v3.2
-                nest_version = "v3.1"
-            else:
-                nest_version = "v3.2"
-
-        except ModuleNotFoundError:
+        if nest_version == "":
             Logger.log_message(None, -1, "An error occurred while importing the `nest` module in Python. Please check your NEST installation-related environment variables and paths, or specify ``nest_version`` manually in the code generator options.", None, LoggingLevel.ERROR)
             sys.exit(1)
 
