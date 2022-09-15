8ce5f19f9ba5ba5ee0acbf8bdf0612c2eb21b477
C.A.P. Linssen <charl@turingbirds.com>
2022-09-15 07:52:56 -0700

    add SpiNNaker reference converter

--- pynestml/codegeneration/printers/spinnaker_reference_converter.py
+++ pynestml/codegeneration/printers/spinnaker_reference_converter.py
@@ -31,3 +31,97 @@ class SpinnakerReferenceConverter(BaseReferenceConverter):
     """
     def name(self, node: Union[VariableSymbol, ASTVariable]) -> str:
         return super().name(node)
+
+    def convert_function_call(self, function_call: ASTFunctionCall, prefix: str = '') -> str:
+        """
+        Converts a single handed over function call to C++ NEST API syntax.
+
+        Parameters
+        ----------
+        function_call
+            The function call node to convert.
+        prefix
+            Optional string that will be prefixed to the function call. For example, to refer to a function call in the class "node", use a prefix equal to "node." or "node->".
+
+            Predefined functions will not be prefixed.
+
+        Returns
+        -------
+        s
+            The function call string in C++ syntax.
+        """
+        function_name = function_call.get_name()
+
+        if function_name == 'and':
+            return '&&'
+
+        if function_name == 'or':
+            return '||'
+
+        if function_name == PredefinedFunctions.TIME_RESOLUTION:
+            return 'neuron->this_h'
+
+        if function_name == PredefinedFunctions.TIME_STEPS:
+            raise Exception("time_steps() function not yet implemented")
+
+        if function_name == PredefinedFunctions.CLIP:
+            # warning: the arguments of this function must swapped and
+            # are therefore [v_max, v_min, v], hence its structure
+            return 'std::min({2!s}, std::max({1!s}, {0!s}))'
+
+        if function_name == PredefinedFunctions.MAX:
+            return 'std::max({!s}, {!s})'
+
+        if function_name == PredefinedFunctions.MIN:
+            return 'std::min({!s}, {!s})'
+
+        if function_name == PredefinedFunctions.EXP:
+            return 'std::exp({!s})'
+
+        if function_name == PredefinedFunctions.LN:
+            return 'std::log({!s})'
+
+        if function_name == PredefinedFunctions.LOG10:
+            return 'std::log10({!s})'
+
+        if function_name == PredefinedFunctions.COSH:
+            return 'std::cosh({!s})'
+
+        if function_name == PredefinedFunctions.SINH:
+            return 'std::sinh({!s})'
+
+        if function_name == PredefinedFunctions.TANH:
+            return 'std::tanh({!s})'
+
+        if function_name == PredefinedFunctions.EXPM1:
+            return 'numerics::expm1({!s})'
+
+        if function_name == PredefinedFunctions.RANDOM_NORMAL:
+            return '(({!s}) + ({!s}) * ' + prefix + 'normal_dev_( nest::get_vp_specific_rng( ' + prefix + 'get_thread() ) ))'
+
+        if function_name == PredefinedFunctions.RANDOM_UNIFORM:
+            return '(({!s}) + ({!s}) * nest::get_vp_specific_rng( ' + prefix + 'get_thread() )->drand())'
+
+        if function_name == PredefinedFunctions.EMIT_SPIKE:
+            return 'send_spike(timer_count, time, neuron_index)'
+
+        if function_name == PredefinedFunctions.PRINT:
+            return 'std::cout << {!s}'
+
+        if function_name == PredefinedFunctions.PRINTLN:
+            return 'std::cout << {!s} << std::endl'
+
+        if function_name == PredefinedFunctions.DELIVER_SPIKE:
+            raise Exception("deliver_spike() function not yet implemented")
+
+        # suppress prefix for misc. predefined functions
+        # check if function is "predefined" purely based on the name, as we don't have access to the function symbol here
+        function_is_predefined = PredefinedFunctions.get_function(function_name)
+        if function_is_predefined:
+            prefix = ''
+
+        if ASTUtils.needs_arguments(function_call):
+            n_args = len(function_call.get_args())
+            return prefix + function_name + '(' + ', '.join(['{!s}' for _ in range(n_args)]) + ')'
+        return prefix + function_name + '()'
+
