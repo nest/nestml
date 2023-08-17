495abdbd8f464c6cfa7c35521e57ad5d94507385
C. Linssen <c.linssen@fz-juelich.de>
2023-08-17 02:25:29 -0700

    add script

--- run_spinnaker_neuron_test_inside_apptainer.sh
+++ run_spinnaker_neuron_test_inside_apptainer.sh
@@ -3,5 +3,5 @@
 # need to create this directory first, otherwise it gets ignored in the PYTHONPATH!
 mkdir $HOME/nestml/spinnaker-install
 
-PYTHONPATH=$HOME/nestml/spinnaker-install:$HOME/spinnaker/SpiNNMachine:$HOME/spinnaker/DataSpecification:/usr/local/lib/python3.10/site-packages python3 -m pytest -s --pdb ./tests/spinnaker_tests/test_spinnaker_iaf_psc_exp.py
+PYTHONPATH=$HOME/nestml/spinnaker-install:/usr/local/lib/python3.10/site-packages python3 -m pytest -s --pdb ./tests/spinnaker_tests/test_spinnaker_iaf_psc_exp.py
 
