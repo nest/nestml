c3cd71e16b26cbeb18379a1de4ccf85ea84c5c3b
Pooja Babu <p.babu@fz.juelich.de>
2023-09-21 12:28:45 +0200

    Fix NEST version detection

--- pynestml/codegeneration/nest_tools.py
+++ pynestml/codegeneration/nest_tools.py
@@ -63,8 +63,9 @@ try:
             nest_version = "v2.20.2"
     else:
         nest_version = "v" + nest.__version__
-        if "post0.dev0" in nest_version:
-            nest_version = "master"
+        if nest_version.startswith("v3.5") or nest_version.startswith("v3.6"):
+            if "post0.dev0" in nest_version:
+                nest_version = "master"
         else:
             if "kernel_status" not in dir(nest):  # added in v3.1
                 nest_version = "v3.0"
