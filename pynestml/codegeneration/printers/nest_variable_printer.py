45efca91dfa526d4d17ecf678cff07a115b092e7
C.A.P. Linssen <charl@turingbirds.com>
2024-07-16 21:35:03 +0200

    access counter fix

--- pynestml/codegeneration/printers/nest_variable_printer.py
+++ pynestml/codegeneration/printers/nest_variable_printer.py
@@ -69,7 +69,8 @@ class NESTVariablePrinter(CppVariablePrinter):
                 # the disadvantage of this approach is that the time the value is to be obtained is not explicitly specified, so we will actually get the value at the end of the min_delay timestep
                 return "((post_neuron_t*)(__target))->get_" + variable.get_alternate_name() + "()"
 
-            return "((post_neuron_t*)(__target))->get_" + _name + "(_tr_t)"
+            # grab the value from the postsynaptic spiking history buffer
+            return "start->" + _name + "_"
 
         if variable.get_name() == PredefinedVariables.E_CONSTANT:
             return "numerics::e"    # from nest::
