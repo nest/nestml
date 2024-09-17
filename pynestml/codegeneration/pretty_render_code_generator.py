aa1a37255c010131de5ba5888ca0ecc9aa557d8c
C.A.P. Linssen <charl@turingbirds.com>
2024-09-17 03:16:52 -0700

    add pretty code rendering feature

--- pynestml/codegeneration/pretty_render_code_generator.py
+++ pynestml/codegeneration/pretty_render_code_generator.py
@@ -19,8 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 from typing import Sequence
+
+import datetime
+import os
 
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
@@ -40,6 +42,14 @@ class PrettyRenderCodeGenerator(CodeGenerator):
     _default_options = {
         "neuron_models": [],
         "synapse_models": [],
+        "templates": {
+            "path": "resources_pretty_render",
+            "model_templates": {
+                "neuron": ["@MODEL_NAME@.html.jinja2"],
+                "synapse": ["@MODEL_NAME@.html.jinja2"]
+            },
+            "module_templates": []
+        },
     }
 
     def generate_code(self, models: Sequence[ASTModel]) -> None:
@@ -49,13 +59,19 @@ class PrettyRenderCodeGenerator(CodeGenerator):
         lexer_fname = os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'extras', 'syntax-highlighting', 'pygments', 'pygments_nestml.py'))
         self.lexer = load_lexer_from_file(lexer_fname, 'NESTMLLexer')
 
+        self.setup_template_env()
+
         for neuron in neurons:
-            self.generate_code_(neuron, "neuron")
+            self.generate_model_code(neuron.get_name(),
+                                     model_templates=self._model_templates["neuron"],
+                                     template_namespace=self._get_neuron_model_namespace(neuron))
 
         for synapse in synapses:
-            self.generate_code_(synapse, "synapse")
+            self.generate_model_code(synapse.get_name(),
+                                     model_templates=self._model_templates["synapse"],
+                                     template_namespace=self._get_synapse_model_namespace(synapse))
 
-    def generate_code_(self, model, model_type: str):
+    def _get_model_namespace(self, model: ASTModel):
         # Read the source code file
         with open(model.file_path, 'r') as f:
             code = f.read()
@@ -64,119 +80,23 @@ class PrettyRenderCodeGenerator(CodeGenerator):
         formatter = HtmlFormatter(full=True, style='colorful')
 
         # Highlight the code
-        result = highlight(code, self.lexer, formatter)
+        highlighted_code_html = highlight(code, self.lexer, formatter)
 
-        result += """
+        namespace = {"highlighted_code_html": highlighted_code_html,
+                     "now": datetime.datetime.utcnow()}
 
-    <div class="circ">⬤ ⬤ ⬤ </div>
+        return namespace
 
-    <style>
-    .circ {
-        position: absolute;
-    """
-        if "neuron" in model_type:
-            result += """
-        top: 47px;
-        left: 80px;"""
+    def _get_neuron_model_namespace(self, neuron: ASTModel):
+        namespace = self._get_model_namespace(neuron)
+        namespace["model_type"] = "neuron"
+        namespace["model_title"] = "Integrate-and-fire NESTML neuron model"
 
-        if "synapse" in model_type:
-            result += """
-        top: 58px;
-        left: 70px;"""
+        return namespace
 
-        result += """
-        transform: translateX(-50%);
-        font-size: .8em; /* Adjust the size as needed */
-        font-family: Inter;
-        font-weight: 500;
-        letter-spacing: .32px;
-        color: hsla(0,0%,100%,.2);
-        padding: 10px 0; /* Optional: Add padding if needed */
-        transform: rotateX(20deg) rotateY(20deg);
-    }
-    </style>
+    def _get_synapse_model_namespace(self, neuron: ASTModel):
+        namespace = self._get_model_namespace(neuron)
+        namespace["model_type"] = "neuron"
+        namespace["model_title"] = "STDP synapse NESTML model"
 
-
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-
-
-    <style>
-    body {
-        padding: 40px;
-        color: #FFFFFF;
-        background-color: #fff;
-    }
-
-    .highlight::before {
-    """
-        if model_type == "neuron":
-            result += """
-        content: "Integrate-and-fire NESTML neuron model";
-    """
-
-        if model_type == "synapse":
-            result += """
-        content: "STDP synapse NESTML model";
-    """
-
-        result += """
-        position: absolute;
-        top: 0;
-        left: 50%;
-        transform: translateX(-52%);
-        text-align: center;
-        width: 100%;
-        font-size: .8em; /* Adjust the size as needed */
-        font-family: Inter;
-        font-weight: 500;
-        letter-spacing: .32px;
-        color: hsla(0,0%,100%,.6);
-        padding: 10px 0; /* Optional: Add padding if needed */
-    }
-
-    .highlight {
-        box-shadow: 0 0 0 1px var(--frame-highlight-border),0 0 0 1.5px var(--frame-shadow-border),0 2.8px 2.2px rgba(0,0,0,.034),0 6.7px 5.3px rgba(0,0,0,.048),0 12.5px 10px rgba(0,0,0,.06),0 22.3px 17.9px rgba(0,0,0,.072),0 41.8px 33.4px rgba(0,0,0,.086),0 100px 80px rgba(0,0,0,.12);
-
-        border-radius: 10px;
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-        transform: rotateX(20deg) rotateY(20deg);
-    """
-        if model_type == "neuron":
-            result += """
-        width: 800px;
-    """
-
-        if model_type == "synapse":
-            result += """
-        width: 500px;
-    """
-
-        result += """
-        padding: 40px 20px 20px 20px;
-        background-color: #2e273f;
-    }
-
-    .mf, .mi {
-        color: #7A7FFD !important;
-    }
-
-    .nb {
-        color: #d3c277 !important;
-    }
-
-    .c1 {
-        color: #807796 !important;
-    }
-
-    .k {
-        color: #FF659C !important;
-    }
-
-    .o {
-        color: #FF659C !important;
-    }
-
-    </style>"""
-
-        with open(str(os.path.join(FrontendConfiguration.get_target_path(), model.get_name())) + '.html', 'w+') as f:
-            f.write(result)
+        return namespace
