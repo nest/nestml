572af948e14b137ef00289a285a65e920529d2e9
C.A.P. Linssen <charl@turingbirds.com>
2025-04-27 22:48:41 +0200

    update version number after release

--- pynestml/__init__.py
+++ pynestml/__init__.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "8.0.1"
+__version__ = "8.0.1-post-dev"
 
 __all__ = ['cocos',
            'codegeneration',
