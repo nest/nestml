ab5304ae1f268ad933dd49320d286ec7b837b4d1
Martin Weih <martin.weih@mailbox.tu-dresden.de>
2025-06-02 19:41:24 +0200

    Add iaf_psc_exp neuron model without additional external I_stim current input

--- tests/spinnaker2_tests/test_spinnaker2_iaf_psc_exp.py
+++ tests/spinnaker2_tests/test_spinnaker2_iaf_psc_exp.py
@@ -7,15 +7,15 @@ from pynestml.frontend.pynestml_frontend import generate_spinnaker2_target
 class TestSpiNNaker2IafPscExp:
     """SpiNNaker2 code generation tests"""
 
-    @pytest.fixture(autouse=True,
-                    scope="module")
+    # @pytest.fixture(autouse=True,
+    #                 scope="module")
     def generate_code(self):
         # codegen_opts = {"neuron_synapse_pairs": [{"neuron": "iaf_psc_exp_neuron",
         #                                           "synapse": "stdp_synapse",
         #                                           "post_ports": ["post_spikes"]}]}
 
         files = [
-            os.path.join("models", "neurons", "iaf_psc_exp_neuron.nestml"),
+            os.path.join("models", "neurons", "iaf_psc_exp_neuron_NO_ISTIM.nestml"),
             # os.path.join("models", "synapses", "stdp_synapse.nestml")
         ]
         input_path = [os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.join(
@@ -24,7 +24,7 @@ class TestSpiNNaker2IafPscExp:
         install_path = "spinnaker2-install"
         logging_level = "DEBUG"
         module_name = "nestmlmodule"
-        suffix = "_nestml"
+        suffix = ''#"_nestml"
         generate_spinnaker2_target(input_path,
                                   target_path=target_path,
                                   install_path=install_path,
