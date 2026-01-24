585ab8b54cadd9de8688d51cd6883faedf8bd7d5
LeanderEwert <leander.ewert@rwth-aachen.de>
2026-01-24 19:42:00 +0100

    merged

--- pynestml/frontend/pynestml_frontend.py
+++ pynestml/frontend/pynestml_frontend.py
@@ -84,7 +84,7 @@ def transformers_from_target_name(target_name: str, options: Optional[Mapping[st
         options = synapse_post_neuron_co_generation.set_options(options)
         transformers.append(synapse_post_neuron_co_generation)
 
-    if target_name.upper() in ["NEST"]:
+    if target_name.upper() == "NEST":
         from pynestml.transformers.synapse_post_neuron_transformer import SynapsePostNeuronTransformer
 
         # co-generate neuron and synapse
