9b682eb92e2bf4a69df1046aca402663fa415d01
C.A.P. Linssen <charl@turingbirds.com>
2026-02-16 00:33:52 +0100

    post transformer cleanup

--- tests/nest_tests/stdp_synapse_test.py
+++ tests/nest_tests/stdp_synapse_test.py
@@ -71,7 +71,7 @@ class TestNestSTDPSynapse:
         input_path = [os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.join(
             os.pardir, os.pardir, s))) for s in files]
         generate_nest_target(input_path=input_path,
-                             target_path="nestml-jit",
+                             target_path="/tmp/nestml-jit",
                              logging_level="INFO",
                              module_name="nestml_jit_module",
                              suffix="_nestml",
@@ -87,7 +87,7 @@ class TestNestSTDPSynapse:
         # generate the "non-jit" model, that relies on ArchivingNode
         generate_nest_target(input_path=os.path.realpath(os.path.join(os.path.dirname(__file__),
                                                                       os.path.join(os.pardir, os.pardir, "models", "neurons", "iaf_psc_exp_neuron.nestml"))),
-                             target_path="nestml-non-jit",
+                             target_path="/tmp/nestml-non-jit",
                              logging_level="INFO",
                              module_name="nestml_non_jit_module",
                              suffix="_nestml_non_jit",
