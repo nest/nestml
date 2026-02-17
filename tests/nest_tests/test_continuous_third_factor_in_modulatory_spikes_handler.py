d8800df39f2e13c117bd9b965aee928568e5212b
C.A.P. Linssen <charl@turingbirds.com>
2026-02-17 15:41:24 +0100

    fix accessing of third factor in modulatory spikes handler in synapse models

--- tests/nest_tests/test_continuous_third_factor_in_modulatory_spikes_handler.py
+++ tests/nest_tests/test_continuous_third_factor_in_modulatory_spikes_handler.py
@@ -28,14 +28,6 @@ import nest
 from pynestml.codegeneration.nest_tools import NESTTools
 from pynestml.frontend.pynestml_frontend import generate_nest_target
 
-try:
-    import matplotlib
-    import matplotlib.pyplot as plt
-
-    TEST_PLOTS = True
-except BaseException:
-    TEST_PLOTS = False
-
 
 @pytest.mark.skipif(NESTTools.detect_nest_version().startswith("v2"),
                     reason="This test does not support NEST 2")
